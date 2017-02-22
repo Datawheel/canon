8841e509a2159294261b7a1bb6c1270124d11500
Dave Landry <landry.dave@gmail.com>
2017-02-22 17:07:44 -0500

    preRenderMiddleware now supports analyzing child elements

--- src/middlewares/preRenderMiddleware.js
+++ src/middlewares/preRenderMiddleware.js
@@ -1,4 +1,12 @@
+import React from "react";
+
 export default (dispatch, components, params) => Promise.all(
   components
-    .reduce((previous, current) => (current.need || []).concat(previous), [])
+    .reduce((arr, component) => {
+      (component.need || []).forEach(n => {
+        if (React.Component.isPrototypeOf(n)) arr = arr.concat(n.need);
+        else if (typeof n === "function") arr.push(n);
+      });
+      return arr;
+    }, [])
     .map(need => dispatch(need(params))));
