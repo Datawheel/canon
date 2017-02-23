02fc34036cff7c20ccb3f52a4189a9b122196a30
Dave Landry <landry.dave@gmail.com>
2017-02-23 12:37:55 -0500

    cleans up SectionTitle code

--- src/components/SectionTitle.jsx
+++ src/components/SectionTitle.jsx
@@ -4,10 +4,16 @@ import "./SectionTitle.css";
 class SectionTitle extends Component {
 
   render() {
+
     const {children} = this.props;
     const {slug} = this.context;
-    console.log("SectionTitle", slug);
-    return <h4 className="sectionTitle"><a name={ slug } href={ `#${ slug }`}>{ children }</a></h4>;
+
+    return (
+      <h4 className="sectionTitle">
+        <a name={ slug } href={ `#${ slug }`}>{ children }</a>
+      </h4>
+    );
+
   }
 
 }
