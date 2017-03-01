bb6685a956dd6dfae5ade85e3d28c6a2f420fca1
Dave Landry <landry.dave@gmail.com>
2017-03-01 16:56:40 -0500

    fixes production start script

--- bin/build.js
+++ bin/build.js
@@ -5,4 +5,4 @@ const shell = require("shelljs");
 process.env.NODE_ENV = "production";
 
 shell.exec(`webpack --colors --config ${__dirname}/../webpack/webpack.config.prod.js`);
-shell.exec(`babel --presets es2015,stage-0 --ignore node_modules,static ${__dirname}/server.js -o ${process.cwd()}/index.js`);
+shell.exec(`babel --presets es2015,stage-0 --ignore static ${__dirname}/server.js -o ${process.cwd()}/index.js`);
