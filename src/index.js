96b3b45454088cb20396a9980a5a4a5ed7fbde25
Dave Landry <landry.dave@gmail.com>
2017-04-26 11:58:31 -0400

    [breaking] removes stat component

--- src/index.js
+++ src/index.js
@@ -5,5 +5,4 @@ export {fetchData} from "./actions/fetchData";
 export {CanonComponent} from "./components/CanonComponent.jsx";
 export {Section, SectionColumns, SectionRows} from "./components/Section.jsx";
 export {SectionTitle} from "./components/SectionTitle.jsx";
-export {Stat} from "./components/Stat.jsx";
 export {TopicTitle} from "./components/TopicTitle.jsx";
