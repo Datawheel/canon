49a1ee97e79f34cb33e167caae777892931dcc77
Dave Landry <landry.dave@gmail.com>
2017-07-31 12:35:58 -0400

    namespaces locale based on package.json name

--- bin/locales.js
+++ bin/locales.js
@@ -2,7 +2,8 @@
 
 const shell = require("shelljs");
 const locales = process.env.CANON_LANGUAGES || "en";
+const {name} = JSON.parse(shell.cat("package.json"));
 
-shell.exec(`i18next app -r -l ${ locales } -n canon -p "i18nKey=\\"([^\\"]+)\\"|t\\(\\"([^\\"]+)\\"" -o ./locales`, code => {
+shell.exec(`i18next app -r -l ${ locales } -n ${name} -p "i18nKey=\\"([^\\"]+)\\"|t\\(\\"([^\\"]+)\\"" -o ./locales`, code => {
   shell.exit(code);
 });
