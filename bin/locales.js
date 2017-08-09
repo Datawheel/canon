8311415e91a0d0efed738e96c10f988b0a11a29a
Dave Landry <landry.dave@gmail.com>
2017-08-09 12:54:10 -0400

    locales script now checks all JSX files in project

--- bin/locales.js
+++ bin/locales.js
@@ -4,6 +4,6 @@ const shell = require("shelljs");
 const locales = process.env.CANON_LANGUAGES || "en";
 const {name} = JSON.parse(shell.cat("package.json"));
 
-shell.exec(`i18next app -r -l ${ locales } -n ${name} -p "i18nKey=\\"([^\\"]+)\\"|t\\(\\"([^\\"]+)\\"" -o ./locales`, code => {
+shell.exec(`i18next ./ --fileFilter '*.jsx' -r -l ${locales} -n ${name} -o ./locales`, code => {
   shell.exit(code);
 });
