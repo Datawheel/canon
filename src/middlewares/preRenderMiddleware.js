3eb8ed1fd84d4e108221be043b7d8622a42e6abc
Dave Landry <landry.dave@gmail.com>
2017-09-27 10:40:08 -0400

    updates React to v16 as well as dependent components

--- src/middlewares/preRenderMiddleware.js
+++ src/middlewares/preRenderMiddleware.js
@@ -6,15 +6,17 @@ export default function preRenderMiddleware(store, {components, params}) {
         dispatch = store.dispatch;
 
   function parseComponents(str) {
-    return components.reduce((arr, component) => {
-      (component[str] || []).forEach(n => {
-        if (React.Component.isPrototypeOf(n)) arr = arr.concat(n[str]);
-        else if (typeof n === "function") arr.push(n);
-      });
-      return arr;
-    }, [])
-    .filter(need => need.key && need.key in data ? false : (data[need.key] = "loading", true))
-    .map(need => dispatch(need(params, store.getState())));
+    return components
+      .reduce((arr, component) => {
+        (component[str] || []).forEach(n => {
+          if (n.WrappedComponent) n = n.WrappedComponent;
+          if (React.Component.isPrototypeOf(n)) arr = arr.concat(n[str] || []);
+          else if (typeof n === "function") arr.push(n);
+        });
+        return arr;
+      }, [])
+      .filter(need => need.key && need.key in data ? false : (data[need.key] = "loading", true))
+      .map(need => dispatch(need(params, store.getState())));
   }
 
   return Promise.all(parseComponents("preneed"))
