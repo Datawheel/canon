5801be7e93b9cd6fcd131480a306981c3732b4a0
Dave Landry <landry.dave@gmail.com>
2017-12-14 16:45:27 -0500

    fixes locales scraper

--- bin/locales.js
+++ bin/locales.js
@@ -3,10 +3,9 @@
 const shell = require("shelljs");
 
 const path = require("path");
-const appDir = process.cwd();
-const binPath = path.join(appDir, "bin/i18next-scanner.config.js");
+const config = path.join(__dirname, "i18next-scanner.config.js");
 
-shell.exec(`i18next-scanner --config ${binPath} '{app,src,node_modules/datawheel-canon/src}/**/*.{js,jsx}'`, code => {
+shell.exec(`i18next-scanner --config ${config} '{app,src,node_modules/datawheel-canon/src}/**/*.{js,jsx}'`, code => {
   shell.echo("");
   shell.exit(code);
 });
