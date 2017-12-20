e0863333d1eeca4aacf2bf35a109987efb7daa49
Dave Landry <landry.dave@gmail.com>
2017-12-20 15:26:54 -0500

    fixes production build babel preset

--- bin/build.js
+++ bin/build.js
@@ -6,4 +6,4 @@ process.env.NODE_ENV = "production";
 
 shell.exec("clear");
 shell.exec(`webpack --progress --colors --hide-modules --config ${__dirname}/../webpack/webpack.config.prod.js`);
-shell.exec(`babel --presets es2015,stage-0 --ignore static ${__dirname}/server.js -o ${process.cwd()}/index.js`);
+shell.exec(`babel --presets env,stage-0 --ignore static ${__dirname}/server.js -o ${process.cwd()}/index.js`);
--- bin/compile.js
+++ bin/compile.js
@@ -6,4 +6,4 @@ process.env.NODE_ENV = "production";
 
 shell.mkdir("-p", `${process.cwd()}/lib`);
 shell.exec(`webpack --progress --colors --hide-modules --config ${__dirname}/webpack.config.js`);
-shell.exec(`babel --presets es2015,stage-0 --ignore node_modules,static ${process.cwd()}/lib/main.js -o ${process.cwd()}/lib/datawheel-canon.js`);
+shell.exec(`babel --presets env,stage-0 --ignore node_modules,static ${process.cwd()}/lib/main.js -o ${process.cwd()}/lib/datawheel-canon.js`);
