7b1ce3148a7fba38d1068e5ccc546ecfe2e877f2
Dave Landry <landry.dave@gmail.com>
2018-02-07 17:47:57 -0500

    fixes blueprint imports for buttons and forms

--- app/pages/Home.jsx
+++ app/pages/Home.jsx
@@ -2,7 +2,7 @@ import React, {Component} from "react";
 import {Link} from "react-router";
 import {translate, Trans} from "react-i18next";
 import "./Home.css";
-
+import {Button} from "@blueprintjs/core";
 import {Activate} from "../../src/";
 
 // import styles from "style.yml";
@@ -27,6 +27,7 @@ class Home extends Component {
         <Trans i18nKey="complexTrans" count={count}>
             Hello <strong>{{name}}</strong>, you have {{count}} unread message. <Link to="/profile/040AF00182">Click here</Link>.
         </Trans>
+        <Button></Button>
         <Activate location={this.props.location} />
       </div>
     );
