09021461580d5f6dce7f72afb1a5d5b97d3a83c9
Jonathan Speiser <jspeis@gmail.com>
2018-04-03 19:59:54 -0400

    adds github button to login component

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -8,6 +8,7 @@ import facebookIcon from "../images/facebook-logo.svg";
 import twitterIcon from "../images/twitter-logo.svg";
 import instagramIcon from "../images/instagram-logo.svg";
 import googleIcon from "../images/google-logo.svg";
+import githubIcon from "../images/github-logo.svg";
 
 import {
   RESET_SEND_FAILURE,
@@ -98,6 +99,7 @@ class Login extends Component {
         { social.length
           ? <div id="socials">
             { social.includes("facebook") ? <a href="/auth/facebook" className="pt-button facebook"><img className="icon" src={facebookIcon} /><span>{ t("Login.Facebook") }</span></a> : null }
+            { social.includes("github") ? <a href="/auth/github" className="pt-button github"><img className="icon" src={githubIcon} /><span>{ t("Login.Github") }</span></a> : null }
             { social.includes("google") ? <a href="/auth/google" className="pt-button google"><img className="icon" src={googleIcon} /><span>{ t("Login.Google") }</span></a> : null }
             { social.includes("twitter") ? <a href="/auth/twitter" className="pt-button twitter"><img className="icon" src={twitterIcon} /><span>{ t("Login.Twitter") }</span></a> : null }
             { social.includes("instagram") ? <a href="/auth/instagram" className="pt-button instagram"><img className="icon" src={instagramIcon} /><span>{ t("Login.Instagram") }</span></a> : null }
