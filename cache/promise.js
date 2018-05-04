5f6f11d0b1d3a72b26a821493d4e534871fc6c6f
Dave Landry <landry.dave@gmail.com>
2018-05-04 11:37:41 -0400

    tweaks example promise cache

--- cache/promise.js
+++ cache/promise.js
@@ -2,6 +2,7 @@ const axios = require("axios");
 
 module.exports = function() {
 
-  return axios.get("https://api.datausa.io/attrs/cip/").then(d => d.data);
+  return axios.get("https://api.datausa.io/attrs/cip/")
+    .then(d => d.data);
 
 };
