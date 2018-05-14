c4087ed1b90e2bc9ca9aeac138d507d7bed65a45
Francisco <git.id@frabarz.cl>
2018-05-14 16:57:28 -0400

    Canon boilerplate for development

---
+++ packages/vizbuilder/app/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+import Vizbuilder from "../../src/index.jsx";
+
+import "./Home.css";
+
+export default class Home extends React.Component {
+	render() {
+		return <Vizbuilder src="https://chilecube.staging.datachile.io" />;
+	}
+}
