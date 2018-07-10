4f6603cc0a7c58d15c7b62025b0c20a4e7f70bd9
Francisco <git.id@frabarz.cl>
2018-07-10 17:26:43 -0400

    Trick to allow the user to test another mondrian server
    Not actually part of Vizbuilder

--- packages/vizbuilder/app/pages/Home.jsx
+++ packages/vizbuilder/app/pages/Home.jsx
@@ -1,11 +1,38 @@
 import React from "react";
+import KonamiCode from "konami-code";
 
 import Vizbuilder from "../../";
 
 import "./Home.css";
 
+const DATAUSA = "https://canon-api.datausa.io";
+
 export default class Home extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      source:
+        typeof window !== "undefined"
+          ? window.localStorage.getItem("source") || DATAUSA
+          : DATAUSA
+    };
+  }
+
+  componentDidMount() {
+    const kc = new KonamiCode();
+    kc.listen(() => {
+      const source = window.prompt(
+        "Please input a Mondrian server URL:",
+        DATAUSA
+      );
+      localStorage.setItem("source", source);
+      this.setState({source: source || DATAUSA});
+    });
+    this.kc = kc;
+  }
+
   render() {
-    return <Vizbuilder src="https://canon-api.datausa.io" />;
+    return <Vizbuilder src={this.state.source} />;
   }
 }
