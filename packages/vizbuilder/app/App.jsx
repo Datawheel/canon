59007a5b2347dfd27f3679a2b4b4f704de880f03
Dave Landry <landry.dave@gmail.com>
2018-08-14 16:16:29 -0400

    updates vizbuilder to use new canon

--- packages/vizbuilder/app/App.jsx
+++ packages/vizbuilder/app/App.jsx
@@ -1,14 +1,13 @@
 import React, {Component} from "react";
-import {Canon} from "datawheel-canon";
 import "./App.css";
 
 export default class App extends Component {
 
   render() {
     return (
-      <Canon>
+      <div>
         { this.props.children }
-      </Canon>
+      </div>
     );
   }
 
