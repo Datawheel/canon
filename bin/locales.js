a6c59b46df34a2fe45a10ff917c5e2cda0c59b5b
Dave Landry <landry.dave@gmail.com>
2018-08-15 12:21:46 -0400

    fixes main canon dev script pathing to match new package name

--- bin/locales.js
+++ bin/locales.js
@@ -5,7 +5,7 @@ const shell = require("shelljs");
 const path = require("path");
 const config = path.join(__dirname, "i18next-scanner.config.js");
 
-shell.exec(`i18next-scanner --config ${config} '{app,src,node_modules/datawheel-canon/src}/**/*.{html,js,jsx}'`, code => {
+shell.exec(`i18next-scanner --config ${config} '{app,src,node_modules/@datawheel/canon-core/src}/**/*.{html,js,jsx}'`, code => {
   shell.echo("");
   shell.exit(code);
 });
