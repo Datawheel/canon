6b2f3b26955783af12638fb8a8d8cca109ce2461
cnavarreteliz <cnavarreteliz@gmail.com>
2018-09-12 12:06:04 -0300

    uses d3-array functions for to calculate deviation and mean

--- packages/vizbuilder/src/helpers/math.js
+++ packages/vizbuilder/src/helpers/math.js
@@ -1,15 +1,4 @@
-/**
- * Calculate the average of a data array
- * This means it should have a numeric value, and a valid operator.
- * @param {Array} data An array to check
- * @param {String} measureName Name of the measure
- */
-export function average(data, measureName) {
-  const len = data.length, 
-        sum = data.reduce((sum, d) => sum + d[measureName], 0);
-  
-  return sum / len;
-}
+import {deviation, mean} from "d3-array";
 
 /**
  * Calculate the Relative Standard Deviation
@@ -18,22 +7,5 @@ export function average(data, measureName) {
  * @param {String} measureName Name of the measure
  */
 export function relativeStdDev(data, measureName) {
-  return standardDeviation(data, measureName) / average(data, measureName);
-}
-
-
-/**
- * Calculate the standard deviation of a data array
- * This means it should have a numeric value, and a valid operator.
- * @param {Array} data An array to check
- * @param {String} measureName Name of the measure
- */
-export function standardDeviation(data, measureName) {
-  const len = data.length, 
-        sum = data.reduce((sum, d) => sum + d[measureName], 0);
-  
-  const avg = sum / len;
-  const diffs = data.reduce((sum, d) => sum + Math.pow(d[measureName] - avg, 2), 0);
-
-  return Math.sqrt(diffs / (len - 1));
+  return deviation(data.map(d => d[measureName])) / mean(data.map(d => d[measureName]));
 }
