87b4c93ab1f481768ccb4912c4186c9e740b1c53
cnavarreteliz <cnavarreteliz@gmail.com>
2018-09-26 09:56:00 -0300

    adds metaChart boolean

--- packages/vizbuilder/src/components/ChartArea/index.jsx
+++ packages/vizbuilder/src/components/ChartArea/index.jsx
@@ -96,14 +96,16 @@ class ChartArea extends React.Component {
     let n = allQueries.length;
 
     while (n--) {
+      const metaChart = metaQueries.includes(allQueries[n]);
       const chartConfigs = createChartConfig(
         allQueries[n],
         allDatasets[n],
         allMembers[n],
         activeChart,
-        generalConfig
+        generalConfig,
+        metaChart
       );
-      const asdf = chartConfigs.map((chartConfig, i) => (
+      const asdf = chartConfigs.map((chartConfig, i) => 
         <ChartCard
           active={chartConfig.key === activeChart}
           key={chartConfig.key}
@@ -112,7 +114,7 @@ class ChartArea extends React.Component {
         >
           <chartConfig.component config={chartConfig.config} />
         </ChartCard>
-      ));
+      );
       chartElements.unshift(...asdf);
     }
 
