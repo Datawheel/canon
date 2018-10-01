71649bb76995e75280fe8f9e439f20348a7c9ade
Francisco Abarzúa <git.id@frabarz.cl>
2018-10-01 01:40:34 -0300

    creates FilterManager and related components

---
+++ packages/vizbuilder/src/components/Sidebar/FilterManager/Filter.js
@@ -0,0 +1,56 @@
+import {uuid} from "d3plus-common";
+import OPERATORS from "../../../helpers/operators";
+
+class Filter {
+  static isValid(filter) {
+    return filter && filter.measure && filter.hasValue;
+  }
+
+  constructor(measure, operator) {
+    this.uuid = uuid();
+    this.measure = measure;
+    this.operator = operator || OPERATORS.EQUAL;
+    this.value = 0;
+  }
+
+  get name() {
+    return this.measure && this.measure.name;
+  }
+
+  get hasValue() {
+    return isFinite(this.value) && !isNaN(this.value);
+  }
+
+  toString() {
+    return (
+      this.measure &&
+      `${this.measure.annotations._key}-${this.operator}-${this.value}`
+    );
+  }
+
+  getClone() {
+    const clone = new Filter(this.measure, this.operator);
+    clone.uuid = this.uuid;
+    return clone;
+  }
+
+  setMeasure(measure) {
+    const clone = this.getClone();
+    clone.measure = measure;
+    return clone;
+  }
+
+  setOperator(operator) {
+    const clone = this.getClone();
+    clone.operator = operator;
+    return clone;
+  }
+
+  setValue(value) {
+    const clone = this.getClone();
+    clone.value = value;
+    return clone;
+  }
+}
+
+export default Filter;
---
+++ packages/vizbuilder/src/components/Sidebar/FilterManager/FilterItem.jsx
@@ -0,0 +1,125 @@
+import {Button, NumericInput} from "@blueprintjs/core";
+import PropTypes from "prop-types";
+import React from "react";
+
+import {composePropertyName} from "../../../helpers/formatting";
+import OPERATORS, {KIND_NUMBER as NUMBER_OPERATORS, LABELS as OPERATOR_LABELS} from "../../../helpers/operators";
+
+import ConditionPropertySelect from "../ConditionPropertySelect";
+import SidebarCRUDItem from "../SidebarCRUDItem";
+import Filter from "./Filter";
+
+/**
+ * @augments React.Component<IProps,IState>
+ */
+class FilterItem extends SidebarCRUDItem {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isOpen: props.item.new,
+      newItem: null
+    };
+
+    this.handleSetMeasure = this.handleUpdate.bind(this, "setMeasure");
+    this.handleSetOperator = this.handleUpdate.bind(this, "setOperator");
+    this.handleSetValue = this.handleUpdate.bind(this, "setValue");
+  }
+
+  render() {
+    return this.state.isOpen
+      ? this.renderEditable.call(this)
+      : this.renderClosed.call(this);
+  }
+
+  renderClosed() {
+    const {item} = this.props;
+    return (
+      <div className="filter-item">
+        <div className="filter-name">{composePropertyName(item.measure)}</div>
+        <div className="filter-value">{item.value}</div>
+        <div className="filter-actions">
+          <Button
+            text="Delete"
+            className="pt-small"
+            onClick={this.handleDelete}
+          />
+          <Button
+            text={"Edit params"}
+            className="pt-small pt-intent-primary"
+            onClick={this.handleEdit}
+          />
+        </div>
+      </div>
+    );
+  }
+
+  renderEditable() {
+    const {item, options} = this.props;
+    const activeItem = this.state.newItem || item;
+    return (
+      <div className="filter-item editing">
+        <div className="group filter-measure">
+          <ConditionPropertySelect
+            className="custom-select"
+            value={activeItem.measure}
+            items={options}
+            onItemSelect={this.handleSetMeasure}
+          />
+        </div>
+        <div className="group filter-values pt-control-group">
+          <div className="pt-select pt-fill">
+            <select value={activeItem.operator} onChange={this.handleSetOperator}>
+              {NUMBER_OPERATORS.map(ms => (
+                <option key={ms} value={OPERATORS[ms]}>
+                  {OPERATOR_LABELS[ms]}
+                </option>
+              ))}
+            </select>
+          </div>
+          <NumericInput
+            className="pt-fill"
+            value={activeItem.value}
+            onValueChange={this.handleSetValue}
+          />
+        </div>
+        <div className="group actions">
+          <Button
+            className="pt-small"
+            onClick={activeItem.measure ? this.handleClose : this.handleDelete}
+            text={activeItem.measure ? "Reset" : "Delete"}
+          />
+          <Button
+            className="pt-small pt-intent-primary"
+            disabled={!Filter.isValid(activeItem)}
+            onClick={this.handleApply}
+            text="Apply changes"
+          />
+        </div>
+      </div>
+    );
+  }
+}
+
+FilterItem.propTypes = {
+  item: PropTypes.instanceOf(Filter),
+  onDelete: PropTypes.func,
+  onUpdate: PropTypes.func,
+  options: PropTypes.array
+};
+
+/**
+ * @typedef IProps
+ * @prop {Filter} item
+ * @prop {(filter: Filter) => void} onDelete
+ * @prop {(filter: Filter) => void} onUpdate
+ * @prop {Measure[]} options
+ */
+
+/**
+ * @typedef IState
+ * @prop {boolean} isOpen
+ * @prop {Filter} newItem
+ */
+
+export default FilterItem;
