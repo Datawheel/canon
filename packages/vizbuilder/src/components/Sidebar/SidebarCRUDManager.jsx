477be40587f88c9774391283f24606bc119afddc
Francisco Abarzúa <git.id@frabarz.cl>
2018-09-30 23:25:05 -0300

    creates extensible SidebarCRUDManager and SidebarCRUDItem components

---
+++ packages/vizbuilder/src/components/Sidebar/SidebarCRUDManager.jsx
@@ -0,0 +1,89 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+class SidebarCRUDManager extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.createElement = this.createElement.bind(this);
+    this.updateElement = this.updateElement.bind(this);
+    this.deleteElement = this.deleteElement.bind(this);
+
+    this.targetLabel = null;
+  }
+
+  createElement() {
+    const {context, targetLabel} = this;
+    const {items} = this.props;
+
+    context.loadControl(
+      () => {
+        const item = this.createNewInstance.call(this);
+        return {
+          query: {
+            [targetLabel]: [].concat(items, item)
+          }
+        };
+      },
+      context.generateMetaQueries,
+      context.fetchQuery
+    );
+  }
+
+  updateElement(item) {
+    const {context, targetLabel} = this;
+    const {items} = this.props;
+
+    const index = items.findIndex(obj => obj.uuid === item.uuid);
+    if (index === -1) return;
+
+    context.loadControl(
+      () => {
+        const newItems = items.slice();
+        newItems.splice(index, 1, item);
+        return {
+          query: {
+            [targetLabel]: newItems
+          }
+        };
+      },
+      context.generateMetaQueries,
+      context.fetchQuery
+    );
+  }
+
+  deleteElement(item) {
+    const {context, targetLabel} = this;
+    const {items} = this.props;
+
+    const index = items.findIndex(obj => obj.uuid === item.uuid);
+    if (index === -1) return;
+
+    context.loadControl(
+      () => {
+        const newItems = items.slice();
+        newItems.splice(index, 1);
+        return {
+          query: {
+            [targetLabel]: newItems
+          }
+        };
+      },
+      context.generateMetaQueries,
+      context.fetchQuery
+    );
+  }
+}
+
+SidebarCRUDManager.contextTypes = {
+  generateMetaQueries: PropTypes.func,
+  fetchQuery: PropTypes.func,
+  loadControl: PropTypes.func,
+  stateUpdate: PropTypes.func
+};
+
+SidebarCRUDManager.defaultProps = {
+  items: []
+};
+
+export default SidebarCRUDManager;
