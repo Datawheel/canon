5c83a5ebc4706eadbaf43d4e8c924fb2d266f633
jhmullen <jimmy.mullen@gmail.com>
2018-10-03 12:32:22 -0400

    adds canon scaffolding to cms subdirectory

---
+++ packages/cms/app/pages/Home.jsx
@@ -0,0 +1,15 @@
+import React, {Component} from "react";
+import "./Home.css";
+import Builder from "../../src/Builder";
+
+export default class Home extends Component {
+
+  render() {
+    return (
+      <div id="Home">
+        
+      </div>
+    );
+  }
+
+}
