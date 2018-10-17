39fbe879586984f506e6280263b3f7de62728d7b
jhmullen <jimmy.mullen@gmail.com>
2018-10-17 18:59:41 -0400

    adds search population hook to api route

--- packages/cms/src/api/cmsRoute.js
+++ packages/cms/src/api/cmsRoute.js
@@ -1,6 +1,10 @@
+const {Client} = require("mondrian-rest-client");
+const d3Array = require("d3-array");
 const sequelize = require("sequelize");
 const shell = require("shelljs");
 const Op = sequelize.Op;
+
+const client = new Client(process.env.CANON_LOGICLAYER_CUBE);
 
 const topicTypeDir = "src/components/topics/";
 
@@ -125,6 +129,83 @@ const sortStoryTopic = storytopic => {
   storytopic = storytopic.toJSON();
   ["subtitles", "visualizations", "stats", "descriptions"].forEach(type => storytopic[type].sort(sorter));
   return storytopic;
+};
+
+const formatter = (members, data, dimension, level) => {
+
+  const newData = members.reduce((arr, d) => {
+    const obj = {};
+    obj.id = `${d.key}`;
+    obj.name = d.name;
+    obj.display = d.caption;
+    obj.zvalue = data[obj.id] || 0;
+    obj.dimension = dimension;
+    obj.hierarchy = level;
+    obj.stem = -1;
+    arr.push(obj);
+    return arr;
+  }, []);
+  const st = d3Array.deviation(newData, d => d.zvalue);
+  const average = d3Array.median(newData, d => d.zvalue);
+  newData.forEach(d => d.zvalue = (d.zvalue - average) / st);
+  return newData;
+};
+
+const populateSearch = (profileData, db) => {
+  
+  /**
+   *
+   */
+  async function start() {
+
+    const cubeName = profileData.cubeName;
+    const measure = profileData.measure;
+    const dimension = profileData.dimName;
+
+    const cube = await client.cube(cubeName);
+
+    // const levels = cube.dimensionsByName[dimension].hierarchies[0].levels.slice(1);
+    const levels = cube.dimensionsByName[dimension].hierarchies[0].levels.filter(l => l.name !== "(All)");
+
+    let fullList = [];
+    for (let i = 0; i < levels.length; i++) {
+
+      const level = levels[i];
+      const members = await client.members(level);
+
+      const data = await client.query(cube.query
+        .drilldown(dimension, dimension, level.name)
+        .measure(measure), "jsonrecords")
+        .then(resp => resp.data.data)
+        .then(data => data.reduce((obj, d) => {
+          obj[d[`ID ${level.name}`]] = d[measure];
+          return obj;
+        }, {}));
+
+      fullList = fullList.concat(formatter(members, data, dimension, level.name));
+
+    }
+
+    for (let i = 0; i < fullList.length; i++) {
+      const obj = fullList[i];
+      const {id, dimension, hierarchy} = obj;
+      const [row, created] = await db.search.findOrCreate({
+        where: {id, dimension, hierarchy},
+        defaults: obj
+      });
+      if (created) console.log(`Created: ${row.id} ${row.display}`);
+      else {
+        await row.updateAttributes(obj);
+        console.log(`Updated: ${row.id} ${row.display}`);
+      }
+    }
+
+    shell.exit(0);
+
+  }
+
+  start();
+
 };
 
 module.exports = function(app) {
@@ -345,6 +426,7 @@ module.exports = function(app) {
         db.topics.create({ordering: 0, section_id: section.id}).then(() => {
           db.profiles.findAll(profileReqTreeOnly).then(profiles => {
             profiles = sortProfileTree(profiles);
+            populateSearch(profileData, db);
             res.json(profiles).end();
           });
         });
