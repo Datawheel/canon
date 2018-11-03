ceb14c7d1ae742e03629e66bc159ddbd5555149a
Francisco Abarzúa <git.id@frabarz.cl>
2018-11-03 03:27:53 -0300

    corrects div children of p on DatasetSelect

--- packages/vizbuilder/src/components/Sidebar/DatasetSelect.jsx
+++ packages/vizbuilder/src/components/Sidebar/DatasetSelect.jsx
@@ -3,14 +3,14 @@ import React from "react";
 function DatasetSelect(props) {
   const cubeName = props.value.annotations._cb_name;
   return (
-    <div className="pt-select">
+    <span className="pt-select">
       <select
         className={props.className}
         name="dataset"
         onChange={props.onChange}
         value={cubeName}
       >
-        {props.items.map(item => 
+        {props.items.map(item =>
           <option
             key={item.annotations._cb_name}
             value={item.annotations._cb_name}
@@ -19,7 +19,7 @@ function DatasetSelect(props) {
           </option>
         )}
       </select>
-    </div>
+    </span>
   );
 }
 
