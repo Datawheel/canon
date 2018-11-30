4557dd4fc87ed660ec8e0ed19b5997955715999b
jhmullen <jimmy.mullen@gmail.com>
2018-11-30 12:59:17 -0500

    fixes formatter need by making builder top-level route

--- packages/cms/app/routes.jsx
+++ packages/cms/app/routes.jsx
@@ -2,13 +2,14 @@ import React from "react";
 import {Route, IndexRoute, browserHistory} from "react-router";
 
 import App from "./App";
-import Home from "./pages/Home";
+//import Home from "./pages/Home";
+import Builder from "../src/Builder";
 import Search from "./pages/Search";
 
 export default function RouteCreate() {
   return (
     <Route path="/" component={App} history={browserHistory}>
-      <IndexRoute component={Home} />
+      <IndexRoute component={Builder} />
       <Route path="/search" component={Search} />
     </Route>
   );
