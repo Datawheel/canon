ec9b7b3e4def81f538c3fee10752dbf5ac83be18
Dave Landry <landry.dave@gmail.com>
2018-12-13 20:31:39 -0500

    removes console statement from findYears

--- packages/logiclayer/src/utils/findYears.js
+++ packages/logiclayer/src/utils/findYears.js
@@ -7,7 +7,7 @@ module.exports = function(dimensions) {
 
   const dims = dimensions.filter(d => findName(d).includes("Year") || d.type === "time" || d.annotations && d.annotations.dim_type === "TIME");
   if (!dims.length) return false;
-  console.log(dims);
+
   let dim;
   if (dims.length === 1) dim = dims[0];
   else if (dims.find(d => findName(d) === "Year")) dim = dims.find(d => findName(d) === "Year");
