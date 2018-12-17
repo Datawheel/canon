d9afd96fefa12748d57bf7ce02b2c27632dbc679
Dave Landry <landry.dave@gmail.com>
2018-12-17 18:15:29 -0500

    exports Topic component

--- packages/cms/src/index.js
+++ packages/cms/src/index.js
@@ -2,3 +2,4 @@ export {default as Builder} from "./Builder.jsx";
 
 // Components
 export {default as Search} from "./components/Search/Search.jsx";
+export {default as Topic} from "./components/Topic.jsx";
