896783040cce6f09ea550c9821328c4ce873bfad
Dave Landry <landry.dave@gmail.com>
2018-12-21 15:10:53 -0500

    hooks up sample profile page

--- packages/cms/app/routes.jsx
+++ packages/cms/app/routes.jsx
@@ -2,14 +2,16 @@ import React from "react";
 import {Route, IndexRoute, browserHistory} from "react-router";
 
 import App from "./App";
-//import Home from "./pages/Home";
+import Profile from "./pages/Profile";
 import Builder from "../src/Builder";
 import Search from "./pages/Search";
 
+/** */
 export default function RouteCreate() {
   return (
     <Route path="/" component={App} history={browserHistory}>
       <IndexRoute component={Builder} />
+      <Route path="/profile" component={Profile} />
       <Route path="/search" component={Search} />
     </Route>
   );
