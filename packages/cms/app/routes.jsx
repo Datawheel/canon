3847c36bafe796c07d8bc3026d3ea0288479de6e
jhmullen <jimmy.mullen@gmail.com>
2018-12-26 17:58:28 -0500

    adds temporary testing page and sample formatter

--- packages/cms/app/routes.jsx
+++ packages/cms/app/routes.jsx
@@ -4,12 +4,14 @@ import {Route, IndexRoute, browserHistory} from "react-router";
 import App from "./App";
 import Home from "./pages/Home";
 import Search from "./pages/Search";
+import Bar from "./pages/Bar";
 
 export default function RouteCreate() {
   return (
     <Route path="/" component={App} history={browserHistory}>
       <IndexRoute component={Home} />
       <Route path="/search" component={Search} />
+      <Route path="/bar" component={Bar} />
     </Route>
   );
 }
