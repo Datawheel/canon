b71152fee3db5cccffa5d69bb139ac5f318c6440
jhmullen <jimmy.mullen@gmail.com>
2019-01-11 16:07:40 -0500

    fixes ie bug by transpiling all client js

--- packages/cms/src/api/formatters.js
+++ packages/cms/src/api/formatters.js
@@ -1,3 +1,5 @@
+const buble = require("buble");
+
 module.exports = function(app) {
 
   const {db} = app.settings;
@@ -6,8 +8,16 @@ module.exports = function(app) {
 
     db.formatter.findAll()
       .then(results => {
+        results = results.map(r => {
+          r = r.toJSON();
+          if (r.logic) {
+            let code = buble.transform(r.logic).code; 
+            if (code.startsWith("!")) code = code.slice(1);
+            r.logic = code;
+          }
+          return r;
+        });
         res.json(results).end();
       });
   });
-
 };
