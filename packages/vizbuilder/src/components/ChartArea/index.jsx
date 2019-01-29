c62010cfca429130889ed05f40c46106ddf11cd2
Francisco Abarzúa <git.id@frabarz.cl>
2019-01-29 17:19:41 -0300

    removes custom chart height calculation in favor of simplified approach

--- packages/vizbuilder/src/components/ChartArea/index.jsx
+++ packages/vizbuilder/src/components/ChartArea/index.jsx
@@ -8,39 +8,14 @@ import ChartCard from "./ChartCard";
 
 import "./style.css";
 
-const NO_CHARTS = (
-  <div className="area-chart empty">
-    <NonIdealState visual="error" title="Empty dataset" />
-  </div>
-);
-
 class ChartArea extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = {
-      heightArea: 400,
-      heightToolbar: props.toolbar ? 70 : 0
-    };
-
-    this.recalcAreaCall = undefined;
     this.scrollCall = undefined;
-
-    this.areaNode = undefined;
-    this.areaRef = node => {
-      this.areaNode = node;
-      this.recalcArea(node);
-    };
-    this.toolbarRef = node => {
-      if (node) {
-        const bounds = node.getBoundingClientRect();
-        this.setState({heightToolbar: Math.ceil(bounds.height)});
-      }
-    };
 
     this.handleChartSelect = this.handleChartSelect.bind(this);
     this.handleTimeChange = this.handleTimeChange.bind(this);
-    this.resizeAreaUpdate = this.resizeAreaUpdate.bind(this);
     this.scrollEnsure = this.scrollEnsure.bind(this);
   }
 
@@ -48,22 +23,13 @@ class ChartArea extends React.Component {
     return (
       this.props.activeChart !== nextProps.activeChart ||
       this.props.lastUpdate !== nextProps.lastUpdate ||
-      this.props.selectedTime !== nextProps.selectedTime ||
-      this.state.heightArea !== nextState.heightArea
+      this.props.selectedTime !== nextProps.selectedTime
     );
-  }
-
-  componentDidMount() {
-    window.addEventListener("resize", this.resizeAreaUpdate);
   }
 
   componentDidUpdate() {
     requestAnimationFrame(this.dispatchResize);
     requestAnimationFrame(this.dispatchScroll);
-  }
-
-  componentWillUnmount() {
-    window.removeEventListener("resize", this.resizeAreaUpdate);
   }
 
   dispatchScroll() {
@@ -85,21 +51,6 @@ class ChartArea extends React.Component {
     this.context.stateUpdate({query: {selectedTime}});
   }
 
-  recalcArea(node) {
-    if (node) {
-      const bounds = node.getBoundingClientRect();
-      this.setState({heightArea: Math.floor(bounds.height) - 70});
-    }
-  }
-
-  resizeAreaUpdate() {
-    clearTimeout(this.recalcAreaCall);
-    this.recalcAreaCall = setTimeout(
-      this.recalcArea.bind(this, this.areaNode),
-      400
-    );
-  }
-
   scrollEnsure() {
     clearTimeout(this.scrollCall);
     this.scrollCall = setTimeout(this.dispatchScroll, 400);
@@ -107,11 +58,14 @@ class ChartArea extends React.Component {
 
   render() {
     const {activeChart, charts, selectedTime, toolbar} = this.props;
-    const state = this.state;
 
     const n = charts.length;
     if (n === 0) {
-      return NO_CHARTS;
+      return (
+        <div className="area-chart empty">
+          <NonIdealState visual="error" title="No results" />
+        </div>
+      );
     }
 
     const chartsToRender = activeChart
@@ -126,21 +80,12 @@ class ChartArea extends React.Component {
       isSingle: isSingleChart,
       isUnique: isUniqueChart,
       onTimeChange: this.handleTimeChange,
-      selectedTime,
-      uiheight: state.heightArea - state.heightToolbar,
+      selectedTime
     };
 
     return (
-      <div
-        className="area-chart"
-        onScroll={this.scrollEnsure}
-        ref={this.areaRef}
-      >
-        {toolbar && (
-          <div className="toolbar-wrapper" ref={this.toolbarRef}>
-            {toolbar}
-          </div>
-        )}
+      <div className="area-chart" onScroll={this.scrollEnsure}>
+        {toolbar && <div className="toolbar-wrapper">{toolbar}</div>}
         <div
           className={classNames(
             "wrapper chart-wrapper",
