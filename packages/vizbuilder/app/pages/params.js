497626e4ff8dc70243344787ce84d22c0fae2ad4
Francisco Abarzúa <git.id@frabarz.cl>
2019-02-18 23:20:49 -0300

    updates test app structure to match datausa map/vizb use

---
+++ packages/vizbuilder/app/pages/params.js
@@ -0,0 +1,63 @@
+
+export const ENDPOINT = "https://theodore-api.datausa.io";
+
+export const DEFAULT_TOPOJSON = {
+  "Nation": {
+    topojson: "/topojson/world.json",
+    topojsonId: "id",
+    topojsonKey: "countries"
+  },
+  "State": {
+    topojson: "/topojson/states.json",
+    topojsonId: "id",
+    topojsonKey: "states"
+  },
+  get "Origin State"() {
+    return this.State;
+  },
+  get "Destination State"() {
+    return this.State;
+  },
+  "County": {
+    topojson: "/topojson/counties.json",
+    topojsonId: "id",
+    topojsonKey: "counties"
+  },
+  "Puma": {
+    topojson: "/topojson/pumas.json",
+    topojsonId: "id",
+    topojsonKey: "pumas"
+  },
+  get "PUMA"() {
+    return this.Puma;
+  },
+  "Msa": {
+    topojson: "/topojson/msas.json",
+    topojsonId: "id",
+    topojsonKey: "msas"
+  }
+};
+
+export const DEFAULT_CONFIG = {
+  totalConfig: {
+    fontSize: 10,
+    padding: 5,
+    resize: false,
+    textAnchor: "middle"
+  },
+  confidenceConfig: {
+    fillOpacity: 0.15
+  },
+  ocean: "transparent",
+  projection: "geoAlbersUsa",
+  tiles: false,
+  zoom: true,
+  zoomFactor: 2
+};
+
+export const DEFAULT_PERMAKEYS = {
+  measure: "msr",
+  groups: "grp",
+  filters: "fil",
+  enlarged: "show"
+};
--- packages/vizbuilder/app/routes.jsx
+++ packages/vizbuilder/app/routes.jsx
@@ -3,11 +3,15 @@ import {Route, IndexRoute, browserHistory} from "react-router";
 
 import App from "./App";
 import Home from "./pages/Home";
+import Mapa from "./pages/Map";
+import Visualize from "./pages/Visualize";
 
 export default function RouteCreate() {
   return (
     <Route path="/" component={App} history={browserHistory}>
       <IndexRoute component={Home} />
+      <Route path="map" component={Mapa} />
+      <Route path="visualize" component={Visualize} />
     </Route>
   );
 }
