0ba32b4969aa4f1eb0de4bcca3c3884ab7280396
Dave Landry <landry.dave@gmail.com>
2019-02-22 15:46:14 -0500

    adds support for default_year annotation

--- packages/logiclayer/src/utils/findYears.js
+++ packages/logiclayer/src/utils/findYears.js
@@ -1,3 +1,5 @@
+const yn = require("yn");
+
 /** */
 function findName(d) {
   return d.name || d.dimension;
@@ -5,11 +7,18 @@ function findName(d) {
 
 module.exports = function(dimensions) {
 
-  const dims = dimensions.filter(d => findName(d).includes("Year") || d.type === "time" || d.dimensionType === 1 || d.annotations && d.annotations.dim_type === "TIME");
+  const dims = dimensions.filter(d =>
+    findName(d).includes("Year") ||
+    d.type === "time" ||
+    d.dimensionType === 1 ||
+    d.annotations && d.annotations.dim_type === "TIME"
+  );
+
   if (!dims.length) return false;
 
   let dim;
   if (dims.length === 1) dim = dims[0];
+  else if (dims.find(d => yn(d.annotations.default_year))) dim = dims.find(d => yn(d.annotations.default_year));
   else if (dims.find(d => findName(d) === "Year")) dim = dims.find(d => findName(d) === "Year");
   else if (dims.find(d => findName(d).includes("End"))) dim = dims.find(d => findName(d).includes("End"));
   else if (dims.find(d => findName(d).includes("Year"))) dim = dims.find(d => findName(d).includes("Year"));
