e52f6422b519978d8d64a071811285706ba03562
Dave Landry <landry.dave@gmail.com>
2019-04-17 15:20:27 -0400

    switches start script to use pm2

--- bin/start.js
+++
@@ -1,8 +0,0 @@
-#! /usr/bin/env node
-
-const shell = require("shelljs");
-
-process.env.NODE_ENV = "production";
-
-if (!shell.test("-f", `${process.cwd()}/index.js`)) shell.exec("canon-build");
-shell.exec(`\n\nnode ${process.cwd()}/index.js`);
