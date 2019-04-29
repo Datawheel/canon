1d0ff95c20c929ca51c242d28cb03aad1ecad61f
jhmullen <jimmy.mullen@gmail.com>
2019-04-29 16:00:25 -0400

    fixes nested request and adds topic migration

--- packages/cms/scripts/migrate-db.js
+++ packages/cms/scripts/migrate-db.js
@@ -48,15 +48,16 @@ const migrate = async() => {
     descriptions: "topic_description",
     subtitles: "topic_subtitle",
     visualizations: "topic_visualization",
-    stats: "topic_stat"
+    stats: "topic_stat",
+    selectors: "selector"
   };
   
   // Copy the non-cms tables wholesale
-  ["images", "search"].forEach(async table => {
+  for (const table of ["images", "search"]) {
     let rows = await dbold[table].findAll();
     rows = rows.map(row => row.toJSON());
     await dbnew[table].bulkCreate(rows);
-  });
+  }
 
   // Copy each profile
   let profiles = await dbold.profiles.findAll({include: [
@@ -65,10 +66,20 @@ const migrate = async() => {
     {association: "descriptions", separate: true},
     {association: "stats", separate: true},
     {association: "visualizations", separate: true},
-    {association: "sections", separate: true, include: [{association: "topics", separate: true}]}
+    {association: "sections", separate: true, include: [
+      {association: "descriptions", separate: true},
+      {association: "subtitles", separate: true},
+      {association: "topics", separate: true, include: [
+        {association: "selectors", separate: true},
+        {association: "descriptions", separate: true},
+        {association: "subtitles", separate: true},
+        {association: "stats", separate: true},
+        {association: "visualizations", separate: true}
+      ]}
+    ]}
   ]});
   profiles = profiles.map(profile => profile.toJSON());
-  profiles.forEach(async oldprofile => {
+  for (const oldprofile of profiles) { 
     // initiate the topic ordering head counter
     let nextTopicLoc = 0;
     // make the top-level profile itself
@@ -80,16 +91,16 @@ const migrate = async() => {
     await dbnew.profile_content.create({id: newprofile.id, lang: "en", title, subtitle, label}).catch(catcher);
 
     // transfer generators
-    oldprofile.generators.forEach(async generator => {
+    for (const generator of oldprofile.generators) {
       const {name, api, description, logic} = generator;
       await dbnew.generator.create({profile_id: newprofile.id, name, api, description, logic, simple: false}).catch(catcher);
-    });
+    }
 
     // transfer materializers
-    oldprofile.materializers.forEach(async materializer => {
+    for (const materializer of oldprofile.materializers) {
       const {name, description, logic, ordering} = materializer;
       await dbnew.materializer.create({profile_id: newprofile.id, name, description, logic, ordering}).catch(catcher);
-    });
+    }
 
     // make a topic to replace the profile about/stats/viz
     let profiletopic = await dbnew.topic.create({ordering: nextTopicLoc, profile_id: newprofile.id, type: "About", slug: "about"}).catch(catcher);
@@ -98,18 +109,19 @@ const migrate = async() => {
     nextTopicLoc++;
     // create its associated english language content
     await dbnew.topic_content.create({title: "About", lang: "en", id: profiletopic.id});
-    ["descriptions", "stats", "visualizations"].forEach(list => {
-      oldprofile[list].forEach(async entity => {
+    for (const list of ["descriptions", "stats", "visualizations"]) {
+      for (const entity of oldprofile[list]) {
         // migrate the array of profile entities to the new "profiletopic"
-        const {ordering, allowed} = entity;
-        let newTopicEntity = await dbnew[tableLookup[list]].create({topic_id: profiletopic.id, ordering, allowed});
+        const {ordering, allowed, logic} = entity;
+        let newTopicEntity = await dbnew[tableLookup[list]].create({topic_id: profiletopic.id, ordering, allowed, logic});
         newTopicEntity = newTopicEntity.toJSON();
         // create associated english content
         const {description, title, subtitle, value, tooltip} = entity;
         if (list !== "visualizations") await dbnew[`${tableLookup[list]}_content`].create({id: newTopicEntity.id, lang: "en", description, title, subtitle, value, tooltip});
-      });
-    });
-    oldprofile.sections.forEach(async oldsection => {
+      }
+    }
+    oldprofile.sections.sort((a, b) => a.ordering - b.ordering);
+    for (const oldsection of oldprofile.sections) {
       // make this section into a new topic, with an ordering of the current "ordering head"
       const {slug, allowed} = oldsection;
       let sectiontopic = await dbnew.topic.create({ordering: nextTopicLoc, profile_id: newprofile.id, type: "Section", slug, allowed});
@@ -119,8 +131,8 @@ const migrate = async() => {
       // create its associated english language content
       const {title} = sectiontopic;
       await dbnew.topic_content.create({title, lang: "en", id: sectiontopic.id});
-      ["descriptions", "subtitles"].forEach(list => {
-        oldsection[list].forEach(async entity => {
+      for (const list of ["descriptions", "subtitles"]) {
+        for (const entity of oldsection[list]) {
           // migrate the array of section entities to the new "sectiontopic"
           const {ordering, allowed} = entity;
           let newTopicEntity = await dbnew[tableLookup[list]].create({topic_id: sectiontopic.id, ordering, allowed});
@@ -128,14 +140,39 @@ const migrate = async() => {
           // create associated english content
           const {description, subtitle} = entity;
           await dbnew[`${tableLookup[list]}_content`].create({id: newTopicEntity.id, lang: "en", description, subtitle});
-        });
-      });
+        }
+      }
       // For every OLD topic that belonged to the section
-    });
-  });
+      oldsection.topics.sort((a, b) => a.ordering - b.ordering);
+      for (const oldtopic of oldsection.topics) {
+        // create a topic that's a child of the new profile
+        const {type, slug, allowed} = oldtopic;
+        let newtopic = await dbnew.topic.create({profile_id: newprofile.id, ordering: nextTopicLoc, type, slug, allowed});
+        newtopic = newtopic.toJSON();
+        // increment the topic head
+        nextTopicLoc++;
+        // create its associated english language content
+        const {title} = oldtopic;
+        await dbnew.topic_content.create({title, lang: "en", id: newtopic.id});
+        for (const list of ["descriptions", "stats", "subtitles", "visualizations", "selectors"]) {
+          for (const entity of oldtopic[list]) {
+            const {ordering, allowed, logic, options, name, type} = entity;
+            let newTopicEntity = await dbnew[tableLookup[list]].create({topic_id: newtopic.id, ordering, allowed, logic, options, name, type, default: entity.default});
+            newTopicEntity = newTopicEntity.toJSON();     
+            // create associated english content
+            const {description, title, subtitle, value, tooltip} = entity;
+            if (list !== "visualizations" && list !== "selectors") await dbnew[`${tableLookup[list]}_content`].create({id: newTopicEntity.id, lang: "en", description, title, subtitle, value, tooltip});
+          }
+        }
+      }
+    }
+  }
+  console.log("Done.");
 };
 
 migrate();
+
+return;
 
 
 
