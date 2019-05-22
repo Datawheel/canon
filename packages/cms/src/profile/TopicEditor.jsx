944c0593a8c8d9399d8c4a999ac1ad3584fe7fd2
jhmullen <jimmy.mullen@gmail.com>
2019-05-22 16:51:32 -0400

    removes old profile scaffold and fixes topiceditor preview bug

--- packages/cms/src/profile/TopicEditor.jsx
+++ packages/cms/src/profile/TopicEditor.jsx
@@ -36,7 +36,7 @@ class TopicEditor extends Component {
     const prevSlugs = prevProps.previews.map(d => d.slug).join();
     const prevIDs = prevProps.previews.map(d => d.id).join();
     const newSlugs = this.props.previews.map(d => d.slug).join();
-    const newIDs = this.previews.map(d => d.id).join();
+    const newIDs = this.props.previews.map(d => d.id).join();
     if (prevSlugs !== newSlugs || prevIDs !== newIDs) {
       this.hitDB.bind(this)(true);
     }
