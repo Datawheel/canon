46961c2bb7d124f06d1d75d962c26da50b949666
James Ferrell <hello@jamesferrell.me>
2019-05-28 17:53:27 -0400

    fixes weird locale text wrapping that's been bothing me since two show and tells ago

--- packages/cms/src/components/cards/Flag.jsx
+++ packages/cms/src/components/cards/Flag.jsx
@@ -1,26 +1,9 @@
 import React, {Component} from "react";
 import ISO6391 from "iso-639-1";
 
-class Flag extends Component {
-
-  constructor(props) {
-    super(props);
-    this.state = {
-
-    };
-  }
-
+export default class Flag extends Component {
   render() {
-
     const {locale} = this.props;
-
-    return (
-      <div className="cms-flag">
-        { ISO6391.getName(locale) }
-      </div>
-    );
+    return ISO6391.getName(locale);
   }
-
 }
-
-export default Flag;
