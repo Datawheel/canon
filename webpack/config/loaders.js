762355e19b616b34152c11dc655c3eda827c2ebe
Márcio Porto <6521281+MarcioPorto@users.noreply.github.com>
2019-06-14 15:17:46 -0500

    adds case insentive regex symbol

--- webpack/config/loaders.js
+++ webpack/config/loaders.js
@@ -57,7 +57,7 @@ module.exports = props => {
       ]
     },
     {
-      test: /\.(png|jpeg|jpg|gif|bmp|tif|tiff|svg|woff|woff2|eot|ttf|JPG)$/,
+      test: /\.(png|jpeg|jpg|gif|bmp|tif|tiff|svg|woff|woff2|eot|ttf)$/i,
       loader: "url-loader?limit=100000"
     },
     {
