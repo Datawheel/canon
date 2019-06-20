a81fafc4bc6365ddb0372f11dd1126e4a3302be7
James Ferrell <hello@jamesferrell.me>
2019-06-20 12:41:25 -0400

    styles status indicator like a toast

--- packages/cms/src/components/Status.jsx
+++ packages/cms/src/components/Status.jsx
@@ -8,8 +8,8 @@ export default class Status extends Component {
 
     return (
       <p className={`cms-status ${recompiling ? "is-recompiling" : "is-done"}`}>
-        <Icon className="cms-status-icon" icon={ recompiling ? "more" : "tick"} />
-        { recompiling ? "Updating Variables" : "Variables Loaded" }
+        <Icon className="cms-status-icon" icon={ recompiling ? "refresh" : "tick"} />
+        { recompiling ? "Updating variables" : "Variables loaded" }
       </p>
     );
   }
