d2ac6e183c836c40f7794b685dcfa4def1b08675
James Ferrell <hello@jamesferrell.me>
2019-06-21 12:40:55 -0400

    tweaks card style

--- packages/cms/src/components/cards/LocaleName.jsx
+++ packages/cms/src/components/cards/LocaleName.jsx
@@ -1,10 +1,17 @@
 import React, {Component} from "react";
+import {Icon} from "@blueprintjs/core";
 import ISO6391 from "iso-639-1";
+import "./LocaleName.css";
 
 // TODO: make this a helper function instead of a component
 export default class LocaleName extends Component {
   render() {
     const {locale, children} = this.props;
-    return ISO6391.getName(locale || children);
+    return (
+      <h4 className="cms-locale-name font-xxxs">
+        <Icon className="cms-locale-name-icon" icon="translate" />
+        {ISO6391.getName(locale || children)}
+      </h4>
+    );
   }
 }
