18d9c3717bac723d3496da5d3a23b6d430a7b71a
James Ferrell <hello@jamesferrell.me>
2019-06-25 12:47:17 -0400

    cleans up VarTable, fixing weird styling issues, adds boolean ConsoleVariable type

--- packages/cms/src/components/VarTable.jsx
+++ packages/cms/src/components/VarTable.jsx
@@ -1,4 +1,5 @@
 import React, {Component} from "react";
+import {Icon} from "@blueprintjs/core";
 import ConsoleVariable from "./ConsoleVariable";
 import "./VarTable.css";
 
@@ -6,33 +7,26 @@ export default class VarTable extends Component {
   render() {
     const {dataset} = this.props;
 
-    return (
-      <table className="cms-var-table">
-        <tbody className="cms-var-table-body">
-
-          {/* check for display data */}
-          {dataset && (
-            // error
-            dataset.error
-              ? <tr className="cms-var-table-row">
-                <td className="cms-var-table-cell cms-error">
-                  { dataset.error ? dataset.error : "error" }
+    return dataset
+      ? dataset.error || Object.values(dataset).length < 1
+        ? <span className="cms-var-table-error cms-error font-xxs">
+          <Icon className="cms-var-table-error-icon" icon="warning-sign" /> { dataset.error || "No data returned" }
+        </span>
+        : <table className="cms-var-table">
+          <tbody className="cms-var-table-body">
+            {Object.keys(dataset).map(k =>
+              <tr className="cms-var-table-row" key={ k }>
+                <td className="cms-var-table-cell">
+                  { k }:
+                </td>
+                <td className="cms-var-table-cell">
+                  <ConsoleVariable value={ dataset[k] } />
                 </td>
               </tr>
-              // loop through data
-              : Object.keys(dataset).map(k =>
-                <tr className="cms-var-table-row" key={ k }>
-                  <td className="cms-var-table-cell">
-                    { k }:
-                  </td>
-                  <td className="cms-var-table-cell">
-                    <ConsoleVariable value={ dataset[k] } />
-                  </td>
-                </tr>
-              )
-          )}
-        </tbody>
-      </table>
-    );
+            )}
+          </tbody>
+        </table>
+      : null
+    ;
   }
 }
