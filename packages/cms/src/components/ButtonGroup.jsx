9b575b4b50097ae23ba598ea7d6f96615649ce1e
James Ferrell <hello@jamesferrell.me>
2019-06-26 15:11:40 -0400

    styles selector editor

--- packages/cms/src/components/ButtonGroup.jsx
+++ packages/cms/src/components/ButtonGroup.jsx
@@ -4,11 +4,11 @@ import "./ButtonGroup.css";
 
 export default class ButtonGroup extends Component {
   render() {
-    const {buttons} = this.props;
+    const {buttons, className} = this.props;
 
 
     return buttons.length &&
-      <div className="cms-button-group">
+      <div className={`cms-button-group ${className || ""}`}>
         {buttons.map(button =>
           <Button key={button.children} {...button} />
         )}
