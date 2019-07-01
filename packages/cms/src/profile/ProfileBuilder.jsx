77beafff9ff9ee209d44514713050da2d7ad3fd5
jhmullen <jimmy.mullen@gmail.com>
2019-07-01 14:03:03 -0400

    fixes red screen on topic click and fixes reload on permalink

--- packages/cms/src/profile/ProfileBuilder.jsx
+++ packages/cms/src/profile/ProfileBuilder.jsx
@@ -450,7 +450,7 @@ class ProfileBuilder extends Component {
     const {localeDefault} = this.props;
     const formatters = this.context.formatters[localeDefault];
     const {stripHTML} = formatters;
-    const variables = variablesHash[currentPid] && variablesHash[currentPid][localeDefault] ? deepClone(variablesHash[currentPid][localeDefault]) : null;
+    const variables = variablesHash[currentPid] && variablesHash[currentPid][localeDefault] ? deepClone(variablesHash[currentPid][localeDefault]) : {};
     str = this.decode(stripHTML(str));
     str = varSwapRecursive({str, selectors}, formatters, variables, query).str;
     return str;
