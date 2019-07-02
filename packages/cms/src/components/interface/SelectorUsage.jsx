f7cac76117ffc71f87f1f6e9f45b50b54a14db86
James Ferrell <hello@jamesferrell.me>
2019-07-02 15:37:38 -0400

    handles SelectorUsage states (no selectors, no inactive selectors, no active selectors)

--- packages/cms/src/components/interface/SelectorUsage.jsx
+++ packages/cms/src/components/interface/SelectorUsage.jsx
@@ -89,82 +89,89 @@ class SelectorUsage extends Component {
 
         <div className="cms-selector-usage-column">
           <h3 className="cms-selector-usage-heading font-sm">Inactive selectors</h3>
-          <ul className="cms-selector-usage-list">
-            {inactiveSelectors.map(s =>
-              <li className="cms-selector-usage-item" key={s.id}>
-                <label className="cms-selector-usage-item-label font-xs">
-                  {s.name}
-                  <Button
-                    className="cms-selector-usage-item-button font-xxxs"
-                    icon="plus"
-                    iconOnly
-                    onClick={this.addItem.bind(this, s.id)}
-                  >
-                    (Make active)
-                  </Button>
-                </label>
-              </li>
-            )}
-          </ul>
+          {inactiveSelectors.length
+            ? <ul className="cms-selector-usage-list">
+              {inactiveSelectors.map(s =>
+                <li className="cms-selector-usage-item" key={s.id}>
+                  <label className="cms-selector-usage-item-label font-xs">
+                    {s.name}
+                    <Button
+                      className="cms-selector-usage-item-button font-xxxs"
+                      icon="plus"
+                      iconOnly
+                      onClick={this.addItem.bind(this, s.id)}
+                    >
+                      (Make active)
+                    </Button>
+                  </label>
+                </li>
+              )}
+            </ul>
+            : <p className="font-xs">All available selectors have been activated</p>
+          }
         </div>
 
         <div className="cms-selector-usage-column">
           <h3 className="cms-selector-usage-heading font-sm">Active selectors</h3>
-          <ol className="cms-selector-usage-list">
 
-            {activeSelectors.map((s, i) =>
-              <li className="cms-card cms-selector-card" key={`${s.id}-usage-card`}>
+          {activeSelectors.length
+            ? <ol className="cms-selector-usage-list">
 
-                {/* header */}
-                <div className="cms-card-heading">
-                  <h3 className="cms-card-heading-text font-sm">{s.name}</h3>
-                  <div className="cms-button-group">
-                    <Button
-                      className="cms-card-heading-button font-xxs"
-                      onClick={this.removeItem.bind(this, s.id)}
-                      icon="cross"
-                      iconOnly
-                    >
-                      remove {s.name} from active selectors
-                    </Button>
-                  </div>
-                </div>
+              {activeSelectors.map((s, i) =>
+                <li className="cms-card cms-selector-card" key={`${s.id}-usage-card`}>
 
-                {s.options.length > 0 &&
-                  <Select
-                    label={s.title}
-                    fontSize="xs"
-                    value={currentValues[s.name]}
-                    onChange={this.onChange.bind(this, s.name)}
-                  >
-                    {s.options.map(option =>
-                      <option
-                        key={`select-option-${option.option}`}
-                        value={option.option}
+                  {/* header */}
+                  <div className="cms-card-heading">
+                    <h3 className="cms-card-heading-text font-sm">{s.name}</h3>
+                    <div className="cms-button-group">
+                      <Button
+                        className="cms-card-heading-button font-xxs"
+                        onClick={this.removeItem.bind(this, s.id)}
+                        icon="cross"
+                        iconOnly
                       >
-                        {typeof variables[option.option] === "object"
-                          ? JSON.stringify(variables[option.option])
-                          : variables[option.option]}
-                      </option>
-                    )}
-                  </Select>
-                }
-
-                {i !== activeSelectors.length - 1 &&
-                  <div className="cms-reorder">
-                    <Button
-                      onClick={() => console.log("set me up jimmyyyy")}
-                      className="cms-reorder-button"
-                      icon="swap-vertical"
-                      iconOnly
-                    >
-                      Swap positioning of current and next selector
-                    </Button>
+                        remove {s.name} from active selectors
+                      </Button>
+                    </div>
                   </div>
-                }
-              </li>
-            )}
-          </ol>
+
+                  {s.options.length > 0 &&
+                    <Select
+                      label={s.title}
+                      fontSize="xs"
+                      value={currentValues[s.name]}
+                      onChange={this.onChange.bind(this, s.name)}
+                    >
+                      {s.options.map(option =>
+                        <option
+                          key={`select-option-${option.option}`}
+                          value={option.option}
+                        >
+                          {typeof variables[option.option] === "object"
+                            ? JSON.stringify(variables[option.option])
+                            : variables[option.option]}
+                        </option>
+                      )}
+                    </Select>
+                  }
+
+                  {i !== activeSelectors.length - 1 &&
+                    <div className="cms-reorder">
+                      <Button
+                        onClick={() => console.log("set me up jimmyyyy")}
+                        className="cms-reorder-button"
+                        icon="swap-vertical"
+                        iconOnly
+                      >
+                        Swap positioning of current and next selector
+                      </Button>
+                    </div>
+                  }
+                </li>
+              )}
+            </ol>
+            : <p className="font-xs">No selectors are currently activated</p>
+          }
         </div>
       </div>
     );
