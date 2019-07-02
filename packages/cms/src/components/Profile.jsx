23fe656c59063e70ecbaef3052c0120ce4c38677
James Ferrell <hello@jamesferrell.me>
2019-07-02 18:42:15 -0400

    groups certain sections together

--- packages/cms/src/components/Profile.jsx
+++ packages/cms/src/components/Profile.jsx
@@ -3,8 +3,12 @@ import React, {Component} from "react";
 import PropTypes from "prop-types";
 import {connect} from "react-redux";
 import {fetchData} from "@datawheel/canon-core";
-import Section from "./sections/Section";
+
 import libs from "../utils/libs";
+import stripP from "../utils/formatters/stripP";
+
+import Section from "./sections/Section";
+import SectionGrouping from "./sections/components/SectionGrouping";
 
 class Profile extends Component {
 
@@ -36,7 +40,6 @@ class Profile extends Component {
   }
 
   onSelector(name, value) {
-
     const {profile, selectors} = this.state;
     const {id, variables} = profile;
     const {locale} = this.props;
@@ -51,22 +54,62 @@ class Profile extends Component {
       .then(resp => {
         this.setState({profile: resp.data, loading: false});
       });
-
   }
 
   render() {
     const {profile, loading} = this.state;
     const {sections} = profile;
 
+    const groupableSections = ["Card", "Column"]; // sections to be grouped together
+    const groupedSections = []; // array for sections to be accumulated into
+
+    // reduce sections into a nested array of groupedSections
+    groupedSections.push(sections.reduce((arr, section) => {
+      if (arr.length === 0) arr.push(section); // push the first one
+      else {
+        const prevType = arr[arr.length - 1].type;
+        const currType = section.type;
+        // if the current and previous types are groupable and the same type, group them into an array
+        if (groupableSections.includes(prevType) && groupableSections.includes(currType) && prevType === currType) {
+          arr.push(section);
+        }
+        // otherwise, push the section as-is
+        else {
+          groupedSections.push(arr);
+          arr = [section];
+        }
+      }
+      return arr;
+    }, []));
+
     return (
-      <div id="Profile">
-        <h1 dangerouslySetInnerHTML={{__html: profile.title}} />
-        <h3 dangerouslySetInnerHTML={{__html: profile.subtitle}} />
-        {sections.map((section, i) => <Section key={`${section.slug}-${i}`} loading={loading} contents={section} />)}
+      <div className="cp">
+        {/* page title TODO: componentize & style */}
+        <header className="cp-header">
+          <div className="cp-header-inner">
+            <h1 className="cp-header-title">{stripP(profile.title)}</h1>
+            {profile.subtitle
+              ? <p className="cp-header-subtitle" dangerouslySetInnerHTML={{__html: stripP(profile.subtitle)}} /> : ""
+            }
+          </div>
+        </header>
+
+        {/* main content sections */}
+        <main className="cp-content" id="main">
+          {groupedSections.map((grouping, i) => grouping.length === 1
+            // ungrouped section
+            ? <Section key={`${grouping[0].slug}-${i}`} loading={loading} contents={grouping[0]} />
+            // grouped sections
+            : <SectionGrouping layout={grouping[0].type}>
+              {grouping.map((section, sectionIndex) =>
+                <Section key={`${section.slug}-${sectionIndex}`} loading={loading} contents={section} />
+              )}
+            </SectionGrouping>
+          )}
+        </main>
       </div>
     );
   }
-
 }
 
 Profile.childContextTypes = {
---
+++ packages/cms/src/components/sections/components/SectionGrouping.jsx
@@ -0,0 +1,15 @@
+import React, {Component} from "react";
+
+export default class SectionGrouping extends Component {
+  render() {
+    const {children, layout} = this.props;
+
+    return (
+      <div className={`cp-section-grouping cp-${layout.toLowerCase()}-section-grouping`}>
+        <div className="cp-section-grouping-inner">
+          {children}
+        </div>
+      </div>
+    );
+  }
+}
