2d67aff1612ca71c6df4c9f5a1770e9a1e749534
jhmullen <jimmy.mullen@gmail.com>
2019-07-11 13:24:20 -0400

    reorganizes migration script hierarchy

---
+++ packages/cms/scripts/db_0.6/storytopic_stat.js
@@ -0,0 +1,33 @@
+module.exports = function(sequelize, db) {
+
+  const s = sequelize.define("storytopic_stat",
+    {
+      id: {
+        type: db.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      storytopic_id: {
+        type: db.INTEGER,
+        onDelete: "cascade",
+        references: {
+          model: "canon_cms_storytopic",
+          key: "id"
+        }
+      },
+      ordering: db.INTEGER
+    }, 
+    {
+      tableName: "canon_cms_storytopic_stat",
+      freezeTableName: true,
+      timestamps: false
+    }
+  );
+
+  s.associate = models => {
+    s.hasMany(models.storytopic_stat_content, {foreignKey: "id", sourceKey: "id", as: "content"});
+  }; 
+
+  return s;
+
+};
