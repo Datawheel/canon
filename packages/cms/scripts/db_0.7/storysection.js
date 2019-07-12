cb3a32f2c7d14d759967ead29aa3b7f490c9fd6a
jhmullen <jimmy.mullen@gmail.com>
2019-07-12 16:04:37 -0400

    adds 0.6 migration and bug fixes

---
+++ packages/cms/scripts/db_0.7/storysection.js
@@ -0,0 +1,45 @@
+module.exports = function(sequelize, db) {
+
+  const s = sequelize.define("storysection",
+    {
+      id: {
+        type: db.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+      },    
+      slug: {
+        type: db.STRING,
+        defaultValue: "new-storysection-slug"
+      },
+      story_id: {
+        type: db.INTEGER,
+        onDelete: "cascade",
+        references: {
+          model: "canon_cms_story",
+          key: "id"
+        }
+      },
+      type: {
+        type: db.STRING,
+        defaultValue: "TextViz"
+      },
+      ordering: db.INTEGER
+    }, 
+    {
+      tableName: "canon_cms_storysection",
+      freezeTableName: true,
+      timestamps: false
+    }
+  );
+
+  s.associate = models => {
+    s.hasMany(models.storysection_content, {foreignKey: "id", sourceKey: "id", as: "content"});
+    s.hasMany(models.storysection_description, {foreignKey: "storysection_id", sourceKey: "id", as: "descriptions"});
+    s.hasMany(models.storysection_stat, {foreignKey: "storysection_id", sourceKey: "id", as: "stats"});
+    s.hasMany(models.storysection_subtitle, {foreignKey: "storysection_id", sourceKey: "id", as: "subtitles"});
+    s.hasMany(models.storysection_visualization, {foreignKey: "storysection_id", sourceKey: "id", as: "visualizations"});
+  };  
+
+  return s;
+
+};
---
+++ packages/cms/scripts/migrate-0.6.js
@@ -0,0 +1,141 @@
+#! /usr/bin/env node
+ 
+const Sequelize = require("sequelize");
+const fs = require("fs");
+const path = require("path");
+
+const oldDBName = process.env.CANON_CONST_MIGRATION_OLD_DB_NAME;
+const oldDBUser = process.env.CANON_CONST_MIGRATION_OLD_DB_USER;
+const oldDBPW = process.env.CANON_CONST_MIGRATION_OLD_DB_PW || null;
+const oldDBHost = process.env.CANON_CONST_MIGRATION_OLD_DB_HOST;
+
+const newDBName = process.env.CANON_CONST_MIGRATION_NEW_DB_NAME;
+const newDBUser = process.env.CANON_CONST_MIGRATION_NEW_DB_USER;
+const newDBPW = process.env.CANON_CONST_MIGRATION_NEW_DB_PW || null;
+const newDBHost = process.env.CANON_CONST_MIGRATION_NEW_DB_HOST;
+
+const dbold = new Sequelize(oldDBName, oldDBUser, oldDBPW, {host: oldDBHost, dialect: "postgres", define: {timestamps: true}, logging: () => {}});
+const dbnew = new Sequelize(newDBName, newDBUser, newDBPW, {host: newDBHost, dialect: "postgres", define: {timestamps: true}, logging: () => {}});
+
+const catcher = e => console.log("error: ", e);
+
+const resetSequence = async(db, modelName, col) => {
+  const maxFetch = await db[modelName].findAll({attributes: [[Sequelize.fn("max", Sequelize.col(col)), "max"]], raw: true});
+  const max = maxFetch ? maxFetch[0].max : null;
+  if (max && typeof max === "number") {
+    const query = `SELECT setval(pg_get_serial_sequence('canon_cms_${modelName}', '${col}'), ${max})`;
+    return db.query(query);
+  }
+  else {
+    return null;
+  }
+};
+
+const loadModels = (db, modelPath, clear) => {
+  const folder = path.join(__dirname, modelPath);
+  fs.readdirSync(folder)
+    .filter(file => file && file.indexOf(".") !== 0)
+    .forEach(file => {
+      const fullPath = path.join(folder, file);
+      const model = db.import(fullPath);
+      db[model.name] = model;
+    });  
+  Object.keys(db).forEach(modelName => {
+    if ("associate" in db[modelName]) db[modelName].associate(db);
+  });
+  if (clear) {
+    return db.sync({force: true}).catch(catcher);
+  }
+  else {
+    return db.sync().catch(catcher);
+  }
+};
+
+const migrate = async() => {
+
+  await loadModels(dbold, "/db_0.6", false);
+  await loadModels(dbnew, "/db_0.7", true);
+
+  // The order of this migration matters - Many tables have FK dependencies, so the tables must be created in order.
+  const migrationMap = [
+    // These ones have no dependencies and can be copied wholesale
+    {old: "formatter", new: "formatter"},
+    {old: "search", new: "search"},
+    {old: "images", new: "images"},
+    // Profile 
+    {old: "profile", new: "profile"},
+    {old: "profile_content", new: "profile_content"},
+    {old: "profile_meta", new: "profile_meta"},
+    {old: "generator", new: "generator"},
+    {old: "materializer", new: "materializer"},
+    // Topic -> Section
+    {old: "topic", new: "section"},
+    {old: "topic_content", new: "section_content"},
+    {old: "topic_description", new: "section_description"},
+    {old: "topic_description_content", new: "section_description_content"},
+    {old: "topic_stat", new: "section_stat"},
+    {old: "topic_stat_content", new: "section_stat_content"},
+    {old: "topic_subtitle", new: "section_subtitle"},
+    {old: "topic_subtitle_content", new: "section_subtitle_content"},
+    {old: "topic_visualization", new: "section_visualization"},
+    // Story
+    {old: "story", new: "story"},
+    {old: "story_content", new: "story_content"},
+    {old: "story_description", new: "story_description"},
+    {old: "story_description_content", new: "story_description_content"},
+    {old: "story_footnote", new: "story_footnote"},
+    {old: "story_footnote_content", new: "story_footnote_content"},
+    // Storytopic -> Storysection
+    {old: "storytopic", new: "storysection"},
+    {old: "storytopic_content", new: "storysection_content"},
+    {old: "storytopic_description", new: "storysection_description"},
+    {old: "storytopic_description_content", new: "storysection_description_content"},
+    {old: "storytopic_stat", new: "storysection_stat"},
+    {old: "storytopic_stat_content", new: "storysection_stat_content"},
+    {old: "storytopic_subtitle", new: "storysection_subtitle"},
+    {old: "storytopic_subtitle_content", new: "storysection_subtitle_content"},
+    {old: "storytopic_visualization", new: "storysection_visualization"}
+  ];
+
+  for (const tableObj of migrationMap) {
+    let rows = await dbold[tableObj.old].findAll();
+    rows = rows.map(row => {
+      row = row.toJSON();
+      Object.keys(row).forEach(key => {
+        if (key.includes("topic")) {
+          row[key.replace("topic", "section")] = row[key];
+          delete row[key];
+        }
+      });
+      return row;
+    });
+    await dbnew[tableObj.new].bulkCreate(rows);
+    await resetSequence(dbnew, tableObj.new, "id");
+  }
+
+  // Selectors need a special migration
+  let selectors = await dbold.selector.findAll();
+  selectors = selectors.map(row => row.toJSON());
+  for (const oldselector of selectors) {
+    let oldtopic = await dbold.topic.findOne({where: {id: oldselector.topic_id}});
+    oldtopic = oldtopic.toJSON();
+    oldselector.profile_id = oldtopic.profile_id;
+    delete oldselector.topic_id;
+    let newselector = await dbnew.selector.create(oldselector);
+    newselector = newselector.toJSON();
+    await dbnew.section_selector.create({
+      section_id: oldtopic.id,
+      selector_id: newselector.id,
+      ordering: oldselector.ordering
+    });
+  }
+
+  console.log("Done.");
+};
+
+migrate();
+
+return;
+
+
+
