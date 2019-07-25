842a49103a4aa06820d1867c126b23145b40d5be
Dave Landry <landry.dave@gmail.com>
2019-07-25 13:46:27 -0400

    moves variables to top-level CanonProvider import

--- src/CanonProvider.jsx
+++ src/CanonProvider.jsx
@@ -8,6 +8,8 @@ import Helmet from "react-helmet";
 import urllite from "urllite";
 import {Portal, Toaster} from "@blueprintjs/core";
 
+import "./variables.css";
+
 import "@blueprintjs/core/lib/css/blueprint.css";
 import "@blueprintjs/icons/lib/css/blueprint-icons.css";
 
