cc9704c6c313baf5092ba84ad7185c1528143130
Dave Landry <landry.dave@gmail.com>
2019-07-30 15:04:03 -0400

    cleans up canon.js

--- canon.js
+++ canon.js
@@ -1,10 +1,10 @@
 /**
-    The object that this file exports is used to set configurations for canon
-    and it's sub-modules.
+  Simple test middleware function
 */
 function exampleReduxMiddleware(canonMiddleware) {
   return canonMiddleware;
 }
+
 module.exports = {
   reduxMiddleware(applyMiddleware, canonMiddleware) {
     console.log("reduxMiddleware from canon.js");
