90aba8333dd47680a9fe4a382826ca466907202e
jhmullen <jimmy.mullen@gmail.com>
2019-08-16 11:11:33 -0400

    moves meta to locale-based table

---
+++ packages/cms/src/db/image_content.js
@@ -0,0 +1,29 @@
+module.exports = function(sequelize, db) {
+
+  const i = sequelize.define("image_content",
+    {
+      id: {
+        type: db.INTEGER,
+        primaryKey: true,
+        onDelete: "cascade",
+        references: {
+          model: "canon_cms_images",
+          key: "id"
+        }
+      },
+      lang: {
+        type: db.STRING,
+        primaryKey: true
+      },
+      meta: db.TEXT
+    },
+    {
+      tableName: "canon_cms_image_content",
+      freezeTableName: true,
+      timestamps: false
+    }
+  );
+
+  return i;
+
+};
--- packages/cms/src/db/images.js
+++ packages/cms/src/db/images.js
@@ -13,7 +13,6 @@ module.exports = function(sequelize, db) {
         unique: true
       },
       author: db.TEXT,
-      meta: db.TEXT,
       license: db.INTEGER
     },
     {
@@ -21,6 +20,10 @@ module.exports = function(sequelize, db) {
       freezeTableName: true
     }
   );
+
+  images.associate = models => {
+    images.hasMany(models.image_content, {foreignKey: "id", sourceKey: "id", as: "content"});
+  };
 
   return images;
 
