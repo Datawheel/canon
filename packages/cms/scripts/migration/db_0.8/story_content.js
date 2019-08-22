af49477ad68fca7d47a4f999d27494dc9b39f277
jhmullen <jimmy.mullen@gmail.com>
2019-08-22 16:47:28 -0400

    adds migration script files

---
+++ packages/cms/scripts/migration/db_0.8/story_content.js
@@ -0,0 +1,40 @@
+module.exports = function(sequelize, db) {
+
+  const s = sequelize.define("story_content",
+    {
+      id: {
+        type: db.INTEGER,
+        primaryKey: true,
+        onDelete: "cascade",
+        references: {
+          model: "canon_cms_story",
+          key: "id"
+        }
+      },
+      locale: {
+        type: db.STRING,
+        primaryKey: true
+      },
+      title: {
+        type: db.STRING,
+        defaultValue: "New Story"
+      },
+      subtitle: {
+        type: db.TEXT,
+        defaultValue: "New Subtitle"
+      },
+      image: {
+        type: db.STRING,
+        defaultValue: "New Image"
+      }
+    }, 
+    {
+      tableName: "canon_cms_story_content",
+      freezeTableName: true,
+      timestamps: false
+    }
+  ); 
+
+  return s;
+
+};
