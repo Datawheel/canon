a42e3865f94e9477971941615045cff1cf0944c1
Richard Hauck <rhauck@deloitte.com>
2019-09-05 16:58:30 -0400

    - Added spacing around cutoff text - Removed unused Parse instance - Set cutoff default to 2 - Added isPercentValues config setting

--- packages/cms/src/components/Viz/PercentageBar.jsx
+++ packages/cms/src/components/Viz/PercentageBar.jsx
@@ -1,8 +1,6 @@
 import axios from "axios";
 import React, {Component} from "react";
 import Button from "../fields/Button";
-import Parse from "../sections/components/Parse";
-
 import "./PercentageBar.css";
 
 class PercentageBar extends Component {
@@ -23,13 +21,14 @@ class PercentageBar extends Component {
     const {dataFormat} = this.props;
 
     const defaults = {
-      cutoff: 5,
+      cutoff: 2,
       cutoffText: false,
       numberFormat: (d, value, total) => {
         const perc = Number(d[value] / total * 100);
         return isNaN(perc) ? "No Data" : `${perc.toFixed(2)}%`;
       },
-      showText: "Show more",
+      isPercentValues: true, // defines whether tick labels are displayed as percentages
+      showText: "Show all",
       hideText: "Hide"
     };
 
@@ -42,8 +41,7 @@ class PercentageBar extends Component {
         if (!config.total) config.total = config.data.reduce((acc, d) => isNaN(d[config.value]) ? acc : acc + Number(d[config.value]), 0);
         this.setState({config});
       });
-    }
-    else {
+    } else {
       config.data = dataFormat(config.data);
       if (!config.total) config.total = config.data.reduce((acc, d) => isNaN(d[config.value]) ? acc : acc + Number(d[config.value]), 0);
       this.setState({config});
@@ -55,76 +53,60 @@ class PercentageBar extends Component {
       this.setState({config: null}, this.buildConfig.bind(this));
     }
   }
-
   render() {
     const {showAll, config} = this.state;
-
     if (!config) return null;
-
-    const {data, cutoff, cutoffText, title, value, groupBy, sort, total, numberFormat, showText, hideText} = config;
-
+    const {data, cutoff, cutoffText, title, value, groupBy, sort, total, numberFormat, isPercentValues, showText, hideText} = config;
     const cutoffFunction = typeof cutoff === "number" ? data => data.slice(0, cutoff) : cutoff;
-
     let displayData = showAll ? data : cutoffFunction(data);
 
     if (sort) displayData = displayData.sort(sort);
 
+    // Create ticks and labels
+    let labelVal, xPos;
+    let lines = [];
+    let ticks = [];
+    for (let i = 0; i <= 10; i++) {
+      // ensure first line isn't cropped left
+      xPos = i === 0 ? '0.1%' : i*10 + '%';
 
+      // Determine whether to display labels as percentages
+      if(isPercentValues){
+        labelVal = i === 0 ? '0%' : Math.round(total*(i*0.1)) + '%';
+      }else{
+        labelVal = i === 0 ? 0 : (total*(i*0.1)).toFixed(2);
+      }
+
+      lines.push(<line key={i} x1={xPos} x2={xPos} y1="0" y2="100%"/>);
+      ticks.push(<text key={i} x={xPos} y="100%">{labelVal}</text>);
+    }
 
     return (
-      <div className="percentage-bar-wrapper">
+      <>
+      <div className={`percentage-bar-wrapper ${isPercentValues ? 'is-percent' : ''}`}>
 
-
-<div className="precentage-bar-grid-holder">
-<svg className="percentage-bar-grid">
+        <div className="precentage-bar-grid-holder">
+          <svg className="percentage-bar-grid">
             <defs>
                 <clipPath id="percentage-bar-clip">
                     <rect x="0" y="0" width="100%" height={showAll ? "99%" : "88%"} />
                 </clipPath>
             </defs>
-
             <g className="percentage-bar-ticks">
-              <line x1="0.1%" x2="0.1%" y1="0" y2="100%"/>
-              <line x1="10%" x2="10%" y1="0" y2="100%"/>
-              <line x1="20%" x2="20%" y1="0" y2="100%"/>
-              <line x1="30%" x2="30%" y1="0" y2="100%"/>
-              <line x1="40%" x2="40%" y1="0" y2="100%"/>
-              <line x1="50%" x2="50%" y1="0" y2="100%"/>
-              <line x1="60%" x2="60%" y1="0" y2="100%"/>
-              <line x1="70%" x2="70%" y1="0" y2="100%"/>
-              <line x1="80%" x2="80%" y1="0" y2="100%"/>
-              <line x1="90%" x2="90%" y1="0" y2="100%"/>
-              <line x1="99.9%" x2="99.9%" y1="0" y2="100%"/>
+              {lines}
             </g>
-
             <g className="percentage-bar-labels">
-                <text x="0" y="100%">0%</text>
-                <text x="10%" y="100%">10%</text>
-                <text x="20%" y="100%">20%</text>
-                <text x="30%" y="100%">30%</text>
-                <text x="40%" y="100%">40%</text>
-                <text x="50%" y="100%">50%</text>
-                <text x="60%" y="100%">60%</text>
-                <text x="70%" y="100%">70%</text>
-                <text x="80%" y="100%">80%</text>
-                <text x="90%" y="100%">90%</text>
-                <text x="100%" y="100%">100%</text>
+              {ticks}
             </g>
           </svg>
-
         </div>
-
-
-
-
 
         <ul className="percentage-bar-list">
           {displayData.filter(d => d).map((d, i) => {
             const percent = d[value] / total * 100;
             const label = d[groupBy];
             return (
-              <React.Fragment key={`percentage-bar-${i}`}>
-                <li className="percentage-bar-item">
+                <li key={`percentage-bar-${i}`} className="percentage-bar-item">
                   <span className="percentage-bar-label label u-font-xs">
                     {label}
                   </span>
@@ -141,10 +123,10 @@ class PercentageBar extends Component {
                   }
 
                 </li>
-              </React.Fragment>
             );
           })}
         </ul>
+        </div>
         <div className="show-more">
           {!showAll && cutoffText &&
             <div className="cutoff-text" dangerouslySetInnerHTML={{__html: cutoffText}}></div>
@@ -160,7 +142,7 @@ class PercentageBar extends Component {
             </Button>
           }
         </div>
-      </div>
+      </>
     );
   }
 }
