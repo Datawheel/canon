7a99b3398b245c42e6558e0b04f7b53e4caddc41
jhmullen <jimmy.mullen@gmail.com>
2019-10-01 16:47:57 -0400

    hotfixes bug with stripEntities dropdown

--- packages/cms/src/utils/formatters/stripEntities.js
+++ packages/cms/src/utils/formatters/stripEntities.js
@@ -2,7 +2,7 @@
 * Removes non breaking spaces & other html entities from a string
 */
 function stripEntities(n) {
-  return String(n).replace(/&([a-z0-9]+|#[0-9]{1,6}|#x[0-9a-f]{1,6});/ig, " ");
+  return typeof n === "string" ? String(n).replace(/&([a-z0-9]+|#[0-9]{1,6}|#x[0-9a-f]{1,6});/ig, " ") : n;
 }
 
 module.exports = stripEntities;
