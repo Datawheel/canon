c1335712caf0131de33707f676ee970644b26ad8
jhmullen <jimmy.mullen@gmail.com>
2019-10-15 21:51:43 -0400

    adds fake endpoint and storyviz fix

--- packages/cms/api/testRoute.js
+++ packages/cms/api/testRoute.js
@@ -11,4 +11,189 @@ module.exports = function(app) {
     ]}).end();
   });
 
+  app.get("/api/obesity", (req, res) => {
+    return res.json({data: [
+      {
+        "County ID": "05000US36011",
+        "County": "Cayuga County",
+        "Year": 2014,
+        "Obesity Rate": 0.25
+      },
+      {
+        "County ID": "05000US36011",
+        "County": "Cayuga County",
+        "Year": 2015,
+        "Obesity Rate": 0.28999999165534973
+      },
+      {
+        "County ID": "05000US36011",
+        "County": "Cayuga County",
+        "Year": 2016,
+        "Obesity Rate": 0.30000001192092896
+      },
+      {
+        "County ID": "05000US36011",
+        "County": "Cayuga County",
+        "Year": 2017,
+        "Obesity Rate": 0.3100000023841858
+      },
+      {
+        "County ID": "05000US36011",
+        "County": "Cayuga County",
+        "Year": 2018,
+        "Obesity Rate": 0.3100000023841858
+      },
+      {
+        "County ID": "05000US36011",
+        "County": "Cayuga County",
+        "Year": 2019,
+        "Obesity Rate": 0.33000001311302185
+      },
+      {
+        "County ID": "05000US36023",
+        "County": "Cortland County",
+        "Year": 2014,
+        "Obesity Rate": 0.27000001072883606
+      },
+      {
+        "County ID": "05000US36023",
+        "County": "Cortland County",
+        "Year": 2015,
+        "Obesity Rate": 0.27000001072883606
+      },
+      {
+        "County ID": "05000US36023",
+        "County": "Cortland County",
+        "Year": 2016,
+        "Obesity Rate": 0.2800000011920929
+      },
+      {
+        "County ID": "05000US36023",
+        "County": "Cortland County",
+        "Year": 2017,
+        "Obesity Rate": 0.27000001072883606
+      },
+      {
+        "County ID": "05000US36023",
+        "County": "Cortland County",
+        "Year": 2018,
+        "Obesity Rate": 0.2800000011920929
+      },
+      {
+        "County ID": "05000US36023",
+        "County": "Cortland County",
+        "Year": 2019,
+        "Obesity Rate": 0.3400000035762787
+      },
+      {
+        "County ID": "05000US36053",
+        "County": "Madison County",
+        "Year": 2014,
+        "Obesity Rate": 0.27000001072883606
+      },
+      {
+        "County ID": "05000US36053",
+        "County": "Madison County",
+        "Year": 2015,
+        "Obesity Rate": 0.2800000011920929
+      },
+      {
+        "County ID": "05000US36053",
+        "County": "Madison County",
+        "Year": 2016,
+        "Obesity Rate": 0.28999999165534973
+      },
+      {
+        "County ID": "05000US36053",
+        "County": "Madison County",
+        "Year": 2017,
+        "Obesity Rate": 0.25999999046325684
+      },
+      {
+        "County ID": "05000US36053",
+        "County": "Madison County",
+        "Year": 2018,
+        "Obesity Rate": 0.2800000011920929
+      },
+      {
+        "County ID": "05000US36053",
+        "County": "Madison County",
+        "Year": 2019,
+        "Obesity Rate": 0.28999999165534973
+      },
+      {
+        "County ID": "05000US36067",
+        "County": "Onondaga County",
+        "Year": 2014,
+        "Obesity Rate": 0.2800000011920929
+      },
+      {
+        "County ID": "05000US36067",
+        "County": "Onondaga County",
+        "Year": 2015,
+        "Obesity Rate": 0.27000001072883606
+      },
+      {
+        "County ID": "05000US36067",
+        "County": "Onondaga County",
+        "Year": 2016,
+        "Obesity Rate": 0.2800000011920929
+      },
+      {
+        "County ID": "05000US36067",
+        "County": "Onondaga County",
+        "Year": 2017,
+        "Obesity Rate": 0.2800000011920929
+      },
+      {
+        "County ID": "05000US36067",
+        "County": "Onondaga County",
+        "Year": 2018,
+        "Obesity Rate": 0.27000001072883606
+      },
+      {
+        "County ID": "05000US36067",
+        "County": "Onondaga County",
+        "Year": 2019,
+        "Obesity Rate": 0.30000001192092896
+      },
+      {
+        "County ID": "05000US36075",
+        "County": "Oswego County",
+        "Year": 2014,
+        "Obesity Rate": 0.3400000035762787
+      },
+      {
+        "County ID": "05000US36075",
+        "County": "Oswego County",
+        "Year": 2015,
+        "Obesity Rate": 0.33000001311302185
+      },
+      {
+        "County ID": "05000US36075",
+        "County": "Oswego County",
+        "Year": 2016,
+        "Obesity Rate": 0.28999999165534973
+      },
+      {
+        "County ID": "05000US36075",
+        "County": "Oswego County",
+        "Year": 2017,
+        "Obesity Rate": 0.28999999165534973
+      },
+      {
+        "County ID": "05000US36075",
+        "County": "Oswego County",
+        "Year": 2018,
+        "Obesity Rate": 0.30000001192092896
+      },
+      {
+        "County ID": "05000US36075",
+        "County": "Oswego County",
+        "Year": 2019,
+        "Obesity Rate": 0.33000001311302185
+      }
+    ]});
+  });
 };
+
