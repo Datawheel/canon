944c24faffc1039a41b975f0dcb3669ac4eb56be
James Ferrell <hello@jamesferrell.me>
2019-10-18 18:01:39 -0400

    handles default section layout when there are no visualizations

--- packages/cms/src/components/sections/Default.jsx
+++ packages/cms/src/components/sections/Default.jsx
@@ -21,15 +21,17 @@ export default class Default extends Component {
         </div>
 
         {/* caption */}
-        <div className={`cp-default-section-figure${
-          visualizations.length > 1 ? " cp-multicolumn-default-section-figure" : ""
-        }${
-          visualizations.filter(viz => viz.logic_simple && viz.logic_simple.type === "Graphic").length ? " cp-graphic-viz-grid" : ""
-        }`}>
-          {visualizations.map((visualization, ii) =>
-            <Viz section={this} config={visualization} headingLevel={vizHeadingLevel} sectionTitle={title} slug={slug} key={ii} />
-          )}
-        </div>
+        {visualizations.length
+          ? <div className={`cp-default-section-figure${
+            visualizations.length > 1 ? " cp-multicolumn-default-section-figure" : ""
+          }${
+            visualizations.filter(viz => viz.logic_simple && viz.logic_simple.type === "Graphic").length ? " cp-graphic-viz-grid" : ""
+          }`}>
+            {visualizations.map((visualization, ii) =>
+              <Viz section={this} config={visualization} headingLevel={vizHeadingLevel} sectionTitle={title} slug={slug} key={ii} />
+            )}
+          </div> : ""
+        }
       </div>
     );
   }
