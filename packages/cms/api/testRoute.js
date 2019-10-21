66268ef1d00e1f3930418196494ee4ba823a55c7
James Ferrell <hello@jamesferrell.me>
2019-10-21 11:47:56 -0400

    replaces custom abbreviate function with d3plus formatAbbreviate; closes #782

--- packages/cms/api/testRoute.js
+++ packages/cms/api/testRoute.js
@@ -1,5 +1,5 @@
 module.exports = function(app) {
-  
+
   app.get("/api/test", (req, res) => {
     res.json({data: [
       {id: "alpha", x: 4, y: 7},
@@ -7,8 +7,10 @@ module.exports = function(app) {
       {id: "alpha", x: 6, y: 13},
       {id: "beta",  x: 4, y: 17},
       {id: "beta",  x: 5, y: 8},
-      {id: "beta",  x: 6, y: 13}
+      {id: "beta",  x: 6, y: 13},
+      // {id: "omega", x: "40000", y: 170000},
+      // {id: "omega", x: "50000", y: 80000},
+      // {id: "omega", x: "60000", y: 130000}
     ]}).end();
   });
 };
-
