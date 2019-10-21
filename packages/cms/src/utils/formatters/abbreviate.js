f7705c0bdb00e8df9cbebbb2a67e3cf3027a2115
James Ferrell <hello@jamesferrell.me>
2019-10-21 11:50:14 -0400

    simplifies passthrough function (#782)

--- packages/cms/src/utils/formatters/abbreviate.js
+++ packages/cms/src/utils/formatters/abbreviate.js
@@ -4,8 +4,4 @@ const {formatAbbreviate} = require("d3plus-format");
  * Abbreviates a number into a smaller more human-readible number.
  * https://github.com/d3plus/d3plus-format/blob/master/src/abbreviate.js#L44
  */
-function abbreviate(n) {
-  return formatAbbreviate(n);
-}
-
-module.exports = abbreviate;
+module.exports = formatAbbreviate;
