e112ab2681355a4deac1d31f2cbd58f5db4a2d3f
James Ferrell <hello@jamesferrell.me>
2019-10-22 15:14:30 -0400

    imports  d3plus formatAbbreviate in a way that doesn't break formatters for CMS users (#782)

--- packages/cms/src/utils/formatters/abbreviate.js
+++ packages/cms/src/utils/formatters/abbreviate.js
@@ -1,7 +1,10 @@
-const {formatAbbreviate} = require("d3plus-format");
+const libs = require("../libs");
 
 /**
  * Abbreviates a number into a smaller more human-readible number.
  * https://github.com/d3plus/d3plus-format/blob/master/src/abbreviate.js#L44
  */
-module.exports = formatAbbreviate;
+function abbreviate(n) {
+  return libs.d3plus.formatAbbreviate(n);
+}
+module.exports = abbreviate;
