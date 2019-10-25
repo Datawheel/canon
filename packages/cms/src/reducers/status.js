809cec73e64129d62dee11cf49281b1798c28e15
jhmullen <jimmy.mullen@gmail.com>
2019-10-25 14:46:57 -0400

    fix bug with deleting profiles

--- packages/cms/src/reducers/status.js
+++ packages/cms/src/reducers/status.js
@@ -2,12 +2,10 @@ const deepClone = require("../utils/deepClone");
 
 export default (status = {}, action) => {
   switch (action.type) {
+    // Basic assign
     case "STATUS_SET": 
       return Object.assign({}, status, action.data);
-    case "VARIABLES_SET": 
-      const newStatus = {variables: deepClone(action.data.variables)};
-      if (action.data.diffCounter) newStatus.diffCounter = action.data.diffCounter;
-      return Object.assign({}, status, newStatus);
+    // When gens/mats are added, force them open for editing. When they are updated, close them.
     case "GENERATOR_NEW": 
       return Object.assign({}, status, {forceID: action.data.id, forceType: "generator", forceOpen: true});
     case "GENERATOR_UPDATE": 
@@ -16,8 +14,17 @@ export default (status = {}, action) => {
       return Object.assign({}, status, {forceID: action.data.id, forceType: "materializer", forceOpen: true});
     case "MATERIALIZER_UPDATE": 
       return Object.assign({}, status, {forceID: false, forceType: false, forceOpen: false});
+    // Updating variables or saving a section means that anything that depends on variables, such as TextCards 
+    // Or the tree, needs to know something changed. Instead of running an expensive stringify on variables,
+    // Just increment a counter that the various cards can subscribe to.
+    case "VARIABLES_SET": 
+      const newStatus = {variables: deepClone(action.data.variables)};
+      if (action.data.diffCounter) newStatus.diffCounter = action.data.diffCounter;
+      return Object.assign({}, status, newStatus);
     case "SECTION_UPDATE": 
       return Object.assign({}, status, {diffCounter: action.diffCounter});
+    case "PROFILE_DELETE": 
+      return Object.assign({}, status, {currentNode: false, currentPid: false});
     default: return status;
   }
 };
