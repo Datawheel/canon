8e77af58e849949cd351204d8dcb43492a7d4155
jhmullen <jimmy.mullen@gmail.com>
2019-11-15 15:49:28 -0500

    adds cmsreducer to index

--- packages/cms/src/index.js
+++ packages/cms/src/index.js
@@ -7,3 +7,6 @@ export {default as Story} from "./components/Story.jsx";
 export {default as StoryLanding} from "./components/StoryLanding.jsx";
 export {default as Section} from "./components/sections/Section.jsx";
 export {default as Search} from "./components/fields/Search.jsx";
+
+// Redux
+export {default as cmsReducer} from "./reducers/index.js";
