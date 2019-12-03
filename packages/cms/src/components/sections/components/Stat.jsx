8d8d2f2359ba6186a73f033496e38c41126af7f9
James Ferrell <hello@jamesferrell.me>
2019-12-03 17:40:14 -0500

    adds length modifier classes to stat elements

--- packages/cms/src/components/sections/components/Stat.jsx
+++ packages/cms/src/components/sections/components/Stat.jsx
@@ -1,10 +1,26 @@
 import React, {Component, Fragment} from "react";
 import {hot} from "react-hot-loader/root";
 
+import stripHTML from "../../../utils/formatters/stripHTML";
 import Parse from "./Parse";
 import "./Stat.css";
 
 class Stat extends Component {
+
+  /** assigns a class based on the length of the string */
+  generateLengthClass(str) {
+    const length = stripHTML(str).length;
+
+    let className = "length-sm";
+
+    if      (length > 40) className = "length-xxl";
+    else if (length > 30) className = "length-xl";
+    else if (length > 20) className = "length-lg";
+    else if (length > 10) className = "length-md";
+
+    return className;
+  }
+
   render() {
     const {className, El, label, value, subtitle} = this.props;
 
@@ -12,7 +28,7 @@ class Stat extends Component {
       <El className={`cp-stat${className ? ` ${className}` : ""}`}>
         {label && <Fragment>
           <span className="cp-stat-label">
-            <Parse El="span" className="cp-stat-label-text label">
+            <Parse El="span" className={`cp-stat-label-text label ${this.generateLengthClass(label)}`}>
               {label}
             </Parse>
           </span>
@@ -20,14 +36,14 @@ class Stat extends Component {
         </Fragment>}
 
         <span className="cp-stat-value">
-          <Parse El="span" className="cp-stat-value-text heading">
+          <Parse El="span" className={`cp-stat-value-text heading ${this.generateLengthClass(value)}`}>
             {value}
           </Parse>
 
           {subtitle && subtitle !== "<p>New Subtitle</p>" &&
             <Fragment>
               <span className="u-visually-hidden">, </span>
-              <Parse El="span" className="cp-stat-subtitle">
+              <Parse El="span" className={`cp-stat-subtitle heading ${this.generateLengthClass(subtitle)}`}>
                 {subtitle}
               </Parse>
             </Fragment>
