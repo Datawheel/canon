f1931b792c95a9bcb83be7a8a391035e2534b68d
Dave Landry <landry.dave@gmail.com>
2020-01-03 14:00:58 -0500

    guards against empty queries in ProfileSearch

--- packages/cms/src/components/fields/ProfileSearch.jsx
+++ packages/cms/src/components/fields/ProfileSearch.jsx
@@ -7,16 +7,23 @@ class ProfileSearch extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      q: "",
-      results: {}
+      query: "",
+      results: false
     };
   }
 
   search() {
-    const {q} = this.state;
-    axios.get(`/api/deepsearch?query=${q}`).then(resp => {
-      this.setState({results: resp.data});
-    });
+    const {query} = this.state;
+
+    if (query) {
+      axios.get(`/api/deepsearch?query=${query}`)
+        .then(resp => {
+          this.setState({results: resp.data});
+        });
+    }
+    else {
+      this.setState({results: false});
+    }
   }
 
   keyPress(e) {
@@ -31,43 +38,47 @@ class ProfileSearch extends Component {
 
   render() {
 
-    const {q, results} = this.state;
+    const {query, results} = this.state;
     const {display} = this.props;
     console.log(results);
 
     return (
-      <div id="Search">
-        <input type="text" value={q} onKeyDown={this.keyPress.bind(this)} onChange={e => this.setState({q: e.target.value})}/>
+      <div className="cms-profilesearch">
+        <input type="text" value={query} onKeyDown={this.keyPress.bind(this)} onChange={e => this.setState({query: e.target.value})}/>
         <button onClick={this.search.bind(this)} >SEARCH</button>
         <div className="cms-profilesearch-container">
-          { (() => {
+          {
+            results
+            ? (() => {
 
-            switch(display) {
+              switch(display) {
 
-              case "columns":
-                return (
-                  <React.Fragment>{Object.keys((results.profiles || {})).map((profile, i) =>
-                    <div key={`p-${i}`}>
-                      <h3>{profile}</h3>
-                      <ul className="cms-profilesearch-list">
-                        {(results.profiles[profile] || []).map((result, j) =>
-                          <li key={`r-${j}`}>{this.linkify(result)}</li>
-                        )}
-                      </ul>
-                    </div>
-                  )}</React.Fragment>
-                );
+                case "columns":
+                  return (
+                    <React.Fragment>{Object.keys((results.profiles || {})).map((profile, i) =>
+                      <div key={`p-${i}`}>
+                        <h3>{profile}</h3>
+                        <ul className="cms-profilesearch-list">
+                          {(results.profiles[profile] || []).map((result, j) =>
+                            <li key={`r-${j}`}>{this.linkify(result)}</li>
+                          )}
+                        </ul>
+                      </div>
+                    )}</React.Fragment>
+                  );
 
-              default:
-                return (
-                  <ul className="cms-profilesearch-list">
-                    {(results.grouped || []).map((result, i) => <li key={`r-${i}`}>{this.linkify(result)}</li>)}
-                  </ul>
-                );
+                default:
+                  return (
+                    <ul className="cms-profilesearch-list">
+                      {(results.grouped || []).map((result, i) => <li key={`r-${i}`}>{this.linkify(result)}</li>)}
+                    </ul>
+                  );
 
-            }
+              }
 
-          })() }
+            })()
+            : <span>Please Search</span>
+          }
         </div>
       </div>
     );
