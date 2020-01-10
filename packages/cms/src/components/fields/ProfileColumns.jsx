93de81cd7a38a9839b202ed42c1911df8227cc0f
Dave Landry <landry.dave@gmail.com>
2020-01-10 11:21:55 -0500

    allows custom HTML in columnTitles of ProfileColumns

--- packages/cms/src/components/fields/ProfileColumns.jsx
+++ packages/cms/src/components/fields/ProfileColumns.jsx
@@ -6,15 +6,16 @@ import "./ProfileColumns.css";
 class ProfileColumns extends Component {
 
   render() {
+
     const {columnTitles, data, tileProps} = this.props;
-    console.log(JSON.stringify([data[0], data[1]]));
+
     return (
       <ul key="columns" className="cms-profilecolumns">
         { data.map((data, i) => {
             const profile = data[0].map(d => d.slug).join("/");
             return (
               <li key={`p-${i}`} className="cms-profilecolumn">
-                <h3 className="cms-profilecolumn-title">{columnTitles[profile] || formatCategory(data)}</h3>
+                <h3 className="cms-profilecolumn-title" dangerouslySetInnerHTML={{__html: columnTitles[profile] || formatCategory(data)}} />
                 <ul className="cms-profilecolumn-list">
                   {data.map((result, j) =>
                     <ProfileTile key={`r-${j}`} {...tileProps} data={result} />)}
