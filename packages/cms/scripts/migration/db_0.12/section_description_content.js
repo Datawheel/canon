d74e0c3b0814f27af457d200bc85dea4b6798e81
jhmullen <jimmy.mullen@gmail.com>
2020-01-16 16:53:58 -0500

    adds 0.12 db and updates search table to include cubename

---
+++ packages/cms/scripts/migration/db_0.12/section_description_content.js
@@ -0,0 +1,32 @@
+module.exports = function(sequelize, db) {
+
+  const t = sequelize.define("section_description_content",
+    {
+      id: {
+        type: db.INTEGER,
+        primaryKey: true,
+        onDelete: "cascade",
+        references: {
+          model: "canon_cms_section_description",
+          key: "id"
+        }
+      },
+      locale: {
+        type: db.STRING,
+        primaryKey: true
+      },
+      description: {
+        type: db.TEXT,
+        defaultValue: ""
+      }
+    }, 
+    {
+      tableName: "canon_cms_section_description_content",
+      freezeTableName: true,
+      timestamps: false
+    }
+  );
+
+  return t;
+
+};
