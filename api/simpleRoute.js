223c321d964264a99e75269983b330f046454591
Dave Landry <landry.dave@gmail.com>
2020-01-29 14:51:17 -0500

    allows custom config for bodyParser json and urlencoded via canon.js

--- api/simpleRoute.js
+++ api/simpleRoute.js
@@ -2,13 +2,19 @@ module.exports = function(app) {
 
   app.get("/api/simple", (req, res) => {
 
-    res.json({simple: false}).end();
+    res.json({simple: false});
 
   });
 
   app.get("/api/slug/:slug", (req, res) => {
 
-    res.json({slug: req.params.slug}).end();
+    res.json({slug: req.params.slug});
+
+  });
+
+  app.post("/api/raw", (req, res) => {
+
+    res.send(req.rawBody).end();
 
   });
 
--- canon.js
+++ canon.js
@@ -6,6 +6,19 @@ function exampleReduxMiddleware(canonMiddleware) {
 }
 
 module.exports = {
+  express: {
+    bodyParser: {
+      json: {
+        verify: (req, red, buf) => {
+          const url = req.originalUrl;
+          console.log("verify", url);
+          if (url.startsWith("/api/raw")) {
+            req.rawBody = buf.toString();
+          }
+        }
+      }
+    }
+  },
   reduxMiddleware(applyMiddleware, canonMiddleware) {
     const message = "reduxMiddleware from canon.js";
     console.log(message);
