bb29a0deae06ddd733098ab2676d6dfe90229643
jhmullen <jimmy.mullen@gmail.com>
2020-02-04 16:44:39 -0500

    makes room for multiples in vizui

--- packages/cms/src/components/editors/components/VizRow.jsx
+++ packages/cms/src/components/editors/components/VizRow.jsx
@@ -22,75 +22,92 @@ class VizRow extends Component {
     const selectedColumns = object.columns || [];
     const allFields = Object.keys(firstObj);
 
+    let values = [];
+    if (method.multiple) {
+      if (typeof object[method.key] === "string") {
+        values = [object[method.key]];
+      }
+      if (Array.isArray(object[method.key])) {
+        values = object[method.key];
+      }
+    }
+    else {
+      values = [object[method.key]];
+    }
+
     return (
       <React.Fragment>
-        {method.format === "Input" && 
-          <TextInput
-            label={method.display}
-            namespace="cms"
-            fontSize="xs"
-            inline
-            key={method.key}
-            value={object[method.key]}
-            onChange={e => onChange(method.key, e)}
-          />
-        }
-        {method.format === "Checkbox" && 
-          <fieldset className="cms-fieldset">
-            <legend className="u-font-sm">Columns</legend>
-            {allFields.map(column =>
-              <label className="cms-checkbox-label u-font-xs" key={column}>
-                <input
-                  type="checkbox"
-                  checked={selectedColumns.includes(column)}
-                  onChange={() => onCheck(column)}
-                /> {column}
-              </label>
-            )}
-          </fieldset>
-        }
-        {(method.format === "Accessor" || method.format === "Variable") &&
-          <React.Fragment>
-            <Select
-              key="cms-key-select"
-              label={ method.multiple 
-                ? <span>
-                  {method.display}
-                  <Button 
-                    onClick={() => onKeyAdd(method.key)}
-                    icon="plus" 
-                    iconOnly
-                  >
-                    Add Grouping
-                  </Button>
-                </span> 
-                : method.display
+        {
+          values.map((value, i) => 
+            <React.Fragment key={`value-${i}`}>
+              {method.format === "Input" && 
+                <TextInput
+                  label={method.display}
+                  namespace="cms"
+                  fontSize="xs"
+                  inline
+                  key={method.key}
+                  value={value}
+                  onChange={e => onChange(method.key, e)}
+                />
               }
-              namespace="cms"
-              fontSize="xs"
-              value={object[method.key]}
-              onChange={e => onChange(method.key, e)}
-              inline
-            >
-              {options}
-            </Select>
-            <Select
-              key="cms-formatter-select"
-              label={`${method.display} formatter`}
-              labelHidden
-              namespace="cms"
-              fontSize="xs"
-              value={object.formatters ? object.formatters[method.key] : "manual-none"}
-              onChange={e => onChangeFormatter(method.key, e)}
-              inline
-            >
-              <option key={null} value="manual-none">No formatter</option>
-              {formatterList.map(f => <option key={f} value={f}>{f}</option>)}
-            </Select>
-          </React.Fragment>
+              {method.format === "Checkbox" && 
+                <fieldset className="cms-fieldset">
+                  <legend className="u-font-sm">Columns</legend>
+                  {allFields.map(column =>
+                    <label className="cms-checkbox-label u-font-xs" key={column}>
+                      <input
+                        type="checkbox"
+                        checked={selectedColumns.includes(column)}
+                        onChange={() => onCheck(column)}
+                      /> {column}
+                    </label>
+                  )}
+                </fieldset>
+              }
+              {(method.format === "Accessor" || method.format === "Variable") &&
+                <React.Fragment>
+                  <Select
+                    key="cms-key-select"
+                    label={ method.multiple 
+                      ? <span>
+                        {method.display}
+                        <Button 
+                          onClick={() => onKeyAdd(method.key)}
+                          icon="plus" 
+                          iconOnly
+                        >
+                          Add Grouping
+                        </Button>
+                      </span> 
+                      : method.display
+                    }
+                    namespace="cms"
+                    fontSize="xs"
+                    value={value}
+                    onChange={e => onChange(method.key, e)}
+                    inline
+                  >
+                    {options}
+                  </Select>
+                  <Select
+                    key="cms-formatter-select"
+                    label={`${method.display} formatter`}
+                    labelHidden
+                    namespace="cms"
+                    fontSize="xs"
+                    value={object.formatters ? object.formatters[method.key] : "manual-none"}
+                    onChange={e => onChangeFormatter(method.key, e)}
+                    inline
+                  >
+                    <option key={null} value="manual-none">No formatter</option>
+                    {formatterList.map(f => <option key={f} value={f}>{f}</option>)}
+                  </Select>
+                </React.Fragment>
+              }
+            </React.Fragment>
+          )
         }
-
-        
       </React.Fragment>
     );
   }
