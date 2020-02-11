30331ddaa434dd900f71939a07f78b8705488074
jhmullen <jimmy.mullen@gmail.com>
2020-02-11 12:31:56 -0500

    adds negative index lookup for urlswap

--- packages/cms/src/utils/urlSwap.js
+++ packages/cms/src/utils/urlSwap.js
@@ -15,11 +15,18 @@ module.exports = function(url, params) {
   const lookup = Object.assign({}, env, params);
 
   (url.match(/<[^\&\=\/>]+>/g) || []).forEach(variable => {
+    // Remove the <> brackets from the edges
     const x = variable.slice(1, -1);
+    // Create an array of accessor strings, e.g. parents[0].ids[3].name => ["parents", "0", "ids", "3", "name"]
     const accessors = x.match(/[^\]\[.]+/g);
     let value;
     try {
-      value = accessors.reduce((o, i) => o[i], lookup);
+      value = accessors.reduce((o, i) => {
+        // If the acccessor is a negative number, attempt a python-esque negative index access pattern
+        const int = parseInt(i, 10);
+        if (!isNaN(int) && int < 0) i = Math.abs((int + o.length) % o.length);
+        return o[i];
+      }, lookup);
     }
     catch (e) {
       if (verbose) console.error("Error in urlSwap: ", e.message);
