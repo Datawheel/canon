5288163f82e5dc56827cecb02c964fd427051d49
jhmullen <jimmy.mullen@gmail.com>
2020-05-06 10:14:21 -0400

    adds migration script for 0.13

---
+++ packages/cms/scripts/migration/db_0.13/profile_content.js
@@ -0,0 +1,40 @@
+module.exports = function(sequelize, db) {
+
+  const p = sequelize.define("profile_content",
+    {
+      id: {
+        type: db.INTEGER,
+        primaryKey: true,
+        onDelete: "cascade",
+        references: {
+          model: "canon_cms_profile",
+          key: "id"
+        }
+      },
+      locale: {
+        type: db.STRING,
+        primaryKey: true
+      },
+      title: {
+        type: db.STRING,
+        defaultValue: "New Profile"
+      },
+      subtitle: {
+        type: db.TEXT,
+        defaultValue: ""
+      },
+      label: {
+        type: db.STRING,
+        defaultValue: ""
+      }
+    },
+    {
+      tableName: "canon_cms_profile_content",
+      freezeTableName: true,
+      timestamps: false
+    }
+  );
+
+  return p;
+
+};
