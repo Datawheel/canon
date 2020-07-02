24b2bfc50cc9e5f44a4cc410f02f3cce4f9a10bb
Dave Landry <landry.dave@gmail.com>
2020-07-02 11:51:16 -0400

    adds d3plus-text strip and titleCase to libs

--- packages/cms/src/utils/libs.js
+++ packages/cms/src/utils/libs.js
@@ -5,11 +5,12 @@ const d3Array = require("d3-array"),
       {formatAbbreviate} = require("d3plus-format"),
       {date} = require("d3plus-axis"),
       {assign, closest, merge} = require("d3plus-common"),
+      {strip, titleCase} = require("d3plus-text"),
       stats = require("./stats");
 
 
 module.exports = {
   d3: Object.assign({}, d3Array, d3Collection, d3Format, d3TimeFormat),
-  d3plus: {assign, closest, date, formatAbbreviate, merge},
+  d3plus: {assign, closest, date, formatAbbreviate, merge, strip, titleCase},
   stats
 };
