2eb47a082eeaf3b566a0c9c08ddb51f9c19a9b45
jhmullen <jimmy.mullen@gmail.com>
2020-07-14 19:43:37 -0400

    fixes spanify to work for formatterse

--- packages/cms/scripts/translation/translate.js
+++ packages/cms/scripts/translation/translate.js
@@ -18,20 +18,20 @@ const source = "en";
 const target = "es";
 
 const faketranslate = s => s.length ? `hola, ${s}` : s;
-const spanify = s => s.length ? s.replace(/\{\{/g, "<span class=\"notranslate\">").replace(/\}\}/g, "</span>") : s;
-const varify = s => s.length ? s.replace(/\<span class\=\"notranslate\"\>([^\<]+)\<\/span\>/g, "{{$1}}") : s;
+const spanify = s => s.length ? s.replace(/([A-z0-9]*\{\{[^\}]+\}\})/g, "<span class=\"notranslate\">$1</span>") : s;
+const varify = s => s.length ? s.replace(/\<span class\=\"notranslate\"\>([A-z0-9]*\{\{[^\}]+\}\})\<\/span\>/g, "$1") : s;
 
 const catcher = e => console.log("error: ", e);
 
 /** */
 async function translateRow(row) {
   const {id, locale, ...rest} = row; //eslint-disable-line
-  const newContent = {id, locale: "gt"};
+  const newContent = {id, locale: "es"};
   const keys = Object.keys(rest);
   for (const key of keys) {
     if (rest[key]) {
       const text = spanify(rest[key]);
-      //let resp;
+      // let resp;
       const resp = await translate.translate(text, target);
       if (resp && resp[0]) {
         newContent[key] = varify(resp[0]);
@@ -51,16 +51,18 @@ async function translateText() {
   const sections = await db.section.findAll({where: {profile_id: 1}}).catch(() => []);
   const sids = sections.map(d => d.id);
   const content = await db.section_content.findAll({where: {id: sids, locale: source}}).catch(() => []);
-  for (const row of content) {
+  for (const [index, row] of content.entries()) {
     const newContent = await translateRow(row.toJSON());
+    console.log(`${index}/${sids.length}`);
     await db.section_content.upsert(newContent).catch(catcher);
   }
   for (const table of ["section_description", "section_stat", "section_subtitle"]) {
     const entities = await db[table].findAll({where: {section_id: sids}}).catch(() => []);
     const cids = entities.map(d => d.id);
     const content = await db[`${table}_content`].findAll({where: {id: cids, locale: source}}).catch(() => []);
-    for (const row of content) {
+    for (const [index, row] of content.entries()) {
       const newContent = await translateRow(row.toJSON());
+      console.log(`${index}/${cids.length}`);
       await db[`${table}_content`].upsert(newContent).catch(catcher);  
     }
   }
