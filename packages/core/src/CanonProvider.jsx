8ccfdf464c5c12f633fe118558a15b42daa9c9c5
Pablo H. Paladino <paladinop@gmail.com>
2020-08-05 18:03:56 -0300

    remove console.log

--- packages/core/src/CanonProvider.jsx
+++ packages/core/src/CanonProvider.jsx
@@ -177,7 +177,6 @@ class CanonProvider extends Component {
 
     const serverRoutes = ["/auth/logout"];
     match({location: url, routes}, (err, redirect, props) => {
-      console.log(url);
       if (err) console.error(err);
       if (serverRoutes.includes(url.pathname)) window.location.href = el.href;
       else if (props) push(`${url.pathname}${url.search}${url.hash}`);
