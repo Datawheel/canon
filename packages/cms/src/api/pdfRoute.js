c4f7375ade319943db50c9eb1d5d45bddfa84cea
Dave Landry <landry.dave@gmail.com>
2020-08-06 15:50:24 -0400

    resizes and styles puppeteer PDF

--- packages/cms/src/api/pdfRoute.js
+++ packages/cms/src/api/pdfRoute.js
@@ -1,14 +1,59 @@
 const puppeteer = require("puppeteer");
 
+const style = `
+<style>
+  .container {
+    color: #444;
+    font-family: 'Helvetica', 'Arial', sans-serif;
+    font-size: 8px;
+    text-align: center;
+    width: 100%;
+  }
+  .pageNumber,
+  .totalPages,
+  .title {
+    font-size: 8px;
+  }
+  .url {
+    font-size: 6px;
+  }
+</style>
+`;
+
 const pdfOptions = {
-  printBackground: true
+  displayHeaderFooter: true,
+  footerTemplate: `
+${style}
+<div class="container">
+  <span class="pageNumber"></span> of <span class="totalPages"></span>
+</div>`,
+  headerTemplate: `
+${style}
+<div class="container">
+  <div class="title"></div>
+  <div class="url"></div>
+</div>`,
+  format: "Letter",
+  margin: {
+    top: 50,
+    right: 50,
+    bottom: 50,
+    left: 50
+  },
+  printBackground: true,
+  scale: 1
 };
 
 const generate = async path => {
   const browser = await puppeteer.launch({headless: true});
   const page = await browser.newPage();
+  await page.setViewport({
+    deviceScaleFactor: 1,
+    height: Math.floor(1100 * 0.75),
+    width: Math.floor(850 * 0.75)
+  });
+  await page.emulateMediaType("print");
   await page.goto(path, {waitUntil: "networkidle2"});
-  page.emulateMediaType("print");
   const pdf = await page.pdf(pdfOptions);
   await browser.close();
   return pdf;
@@ -17,6 +62,7 @@ const generate = async path => {
 module.exports = function(app) {
 
   app.post("/api/pdf", async(req, res) => {
+    res.connection.setTimeout(1000 * 60 * 5);
     const path = `${req.protocol}://${req.headers.host}/${req.body.path}`;
     const pdf = await generate(path);
     res.set({"Content-Type": "application/pdf", "Content-Length": pdf.length});
