ce6503c6255af04ecd47bd60cdffdd87d5b62056
Dave Landry <landry.dave@gmail.com>
2020-09-01 16:00:33 -0400

    allows custom pdfOptions to be passed in body to express endpoint and fixes viewport sizing

--- packages/cms/src/api/pdfRoute.js
+++ packages/cms/src/api/pdfRoute.js
@@ -1,4 +1,5 @@
 const puppeteer = require("puppeteer");
+const {assign} = require("d3plus-common");
 
 const style = `
 <style>
@@ -20,7 +21,7 @@ const style = `
 </style>
 `;
 
-const pdfOptions = {
+const defaultOptions = {
   displayHeaderFooter: true,
   footerTemplate: `
 ${style}
@@ -33,29 +34,35 @@ ${style}
   <div class="title"></div>
   <div class="url"></div>
 </div>`,
-  format: "Letter",
   margin: {
     top: 50,
     right: 50,
     bottom: 50,
     left: 50
   },
-  printBackground: true,
-  scale: 1
+  printBackground: true
 };
 
-const generate = async path => {
-  const browser = await puppeteer.launch({headless: true});
-  const page = await browser.newPage();
-  await page.setViewport({
-    deviceScaleFactor: 1,
-    height: Math.floor(1100 * 0.75),
-    width: Math.floor(850 * 0.75)
+const generate = async(path, userOptions = {}) => {
+
+  const width = 1200;
+  const height = Math.round(width / 8.5 * 11);
+
+  const browser = await puppeteer.launch({
+    args: [
+      `--window-size=${width},${height}`
+    ],
+    defaultViewport: null,
+    headless: true
   });
-  await page.emulateMediaType("print");
+
+  const page = await browser.newPage();
+
   await page.goto(path, {waitUntil: "networkidle2"});
-  const pdf = await page.pdf(pdfOptions);
+  const pdf = await page.pdf(assign({width, height}, defaultOptions, userOptions));
+
   await browser.close();
+
   return pdf;
 };
 
@@ -64,7 +71,7 @@ module.exports = function(app) {
   app.post("/api/pdf", async(req, res) => {
     res.connection.setTimeout(1000 * 60 * 5);
     const path = `${req.protocol}://${req.headers.host}/${req.body.path}`;
-    const pdf = await generate(path);
+    const pdf = await generate(path, req.body.pdfOptions);
     res.set({"Content-Type": "application/pdf", "Content-Length": pdf.length});
     return res.send(pdf);
   });
