3774943b89b89c6ef986cf2e92036213010d7cbe
Dave Landry <landry.dave@gmail.com>
2020-10-27 16:14:21 -0400

    fixes chunkify import usage

--- packages/core/src/helpers/chunkify.js
+++ packages/core/src/helpers/chunkify.js
@@ -2,12 +2,12 @@ import Loading from "$app/components/Loading";
 import loadable from "@loadable/component";
 
 /**
- * @param {Promise} statement The import() statement for the given component. Example: import("@datawheel/canon-cms")
+ * @param {Function} statement An anonymous function that returns the import() statement for the given component. Example: `() => import("@datawheel/canon-cms")`
  * @param {String} [name = "default"] The component name to import, needed for named exports.
  */
 export default (statement, name = "default") => {
   const resolveComponent = components => components[name];
-  const lazy = loadable(() => statement, {fallback: Loading, resolveComponent});
+  const lazy = loadable(statement, {fallback: Loading, resolveComponent});
   lazy.resolveComponent = resolveComponent;
   return lazy;
 };
