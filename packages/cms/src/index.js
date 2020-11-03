143eb831c7d5ff8ca8daa52319047b942aa4733a
Dave Landry <landry.dave@gmail.com>
2020-11-03 09:54:10 -0500

    uses canon-core v0.19 chunkify for Profile and CMS chunks

--- packages/cms/src/index.js
+++ packages/cms/src/index.js
@@ -1,22 +1,36 @@
-export {default as Builder} from "./Builder.jsx";
+import {chunkify} from "@datawheel/canon-core";
 
 // Components
-export {default as Profile} from "./components/Profile.jsx";
-export {default as Mirror} from "./components/Viz/Mirror.jsx";
-export {default as Story} from "./components/Story.jsx";
-export {default as StoryLanding} from "./components/StoryLanding.jsx";
-export {default as Section} from "./components/sections/Section.jsx";
+const Builder = chunkify(/* #__LOADABLE__ */ () => import(/* webpackChunkName: "cms-builder" */ "./Builder.jsx"));
+export {Builder};
+
+const Profile = chunkify(/* #__LOADABLE__ */ () => import(/* webpackChunkName: "cms-profile" */ "./components/Profile.jsx"));
+const Mirror = chunkify(/* #__LOADABLE__ */ () => import(/* webpackChunkName: "cms-profile" */ "./components/Viz/Mirror.jsx"));
+const Section = chunkify(/* #__LOADABLE__ */ () => import(/* webpackChunkName: "cms-profile" */ "./components/sections/Section.jsx"));
+const PDFButton = chunkify(/* #__LOADABLE__ */ () => import(/* webpackChunkName: "cms-profile" */ "./components/sections/components/PDFButton.jsx"));
+const Selector = chunkify(/* #__LOADABLE__ */ () => import(/* webpackChunkName: "cms-profile" */ "./components/sections/components/Selector.jsx"));
+const Stat = chunkify(/* #__LOADABLE__ */ () => import(/* webpackChunkName: "cms-profile" */ "./components/sections/components/Stat.jsx"));
+const Viz = chunkify(/* #__LOADABLE__ */ () => import(/* webpackChunkName: "cms-profile" */ "./components/Viz/Viz.jsx"));
+const StatGroup = chunkify(/* #__LOADABLE__ */ () => import(/* webpackChunkName: "cms-profile" */ "./components/Viz/StatGroup.jsx"));
+export {
+  Profile,
+  Mirror,
+  Section,
+  PDFButton,
+  Selector,
+  Stat,
+  Viz,
+  StatGroup
+};
+
+const Story = chunkify(/* #__LOADABLE__ */ () => import(/* webpackChunkName: "cms-story" */ "./components/Story.jsx"));
+const StoryLanding = chunkify(/* #__LOADABLE__ */ () => import(/* webpackChunkName: "cms-story" */ "./components/StoryLanding.jsx"));
+export {Story, StoryLanding};
+
 export {default as Search} from "./components/fields/Search.jsx";
 export {default as ProfileSearch} from "./components/fields/ProfileSearch.jsx";
 export {default as ProfileColumns} from "./components/fields/ProfileColumns.jsx";
 export {default as ProfileTile} from "./components/fields/ProfileTile.jsx";
-
-// Elements to be used by Custom Sections
-export {default as PDFButton} from "./components/sections/components/PDFButton.jsx";
-export {default as Selector} from "./components/sections/components/Selector.jsx";
-export {default as Stat} from "./components/sections/components/Stat.jsx";
-export {default as Viz} from "./components/Viz/Viz.jsx";
-export {default as StatGroup} from "./components/Viz/StatGroup.jsx";
 
 // Redux
 export {default as cmsReducer} from "./reducers/index.js";
