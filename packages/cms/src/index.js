5ce76796c6400bfd123f51843e25434e061771f3
Dave Landry <landry.dave@gmail.com>
2020-11-03 09:52:27 -0500

    reverts CMS exports to fix canon version mismatch

--- packages/cms/src/index.js
+++ packages/cms/src/index.js
@@ -1,36 +1,22 @@
-import {chunkify} from "@datawheel/canon-core";
+export {default as Builder} from "./Builder.jsx";
 
 // Components
-const Builder = chunkify(() => import(/* webpackChunkName: "cms-builder" */ "./Builder.jsx"));
-export {Builder};
-
-const Profile = chunkify(() => import(/* webpackChunkName: "cms-profile" */ "./components/Profile.jsx"));
-const Mirror = chunkify(() => import(/* webpackChunkName: "cms-profile" */ "./components/Viz/Mirror.jsx"));
-const Section = chunkify(() => import(/* webpackChunkName: "cms-profile" */ "./components/sections/Section.jsx"));
-const PDFButton = chunkify(() => import(/* webpackChunkName: "cms-profile" */ "./components/sections/components/PDFButton.jsx"));
-const Selector = chunkify(() => import(/* webpackChunkName: "cms-profile" */ "./components/sections/components/Selector.jsx"));
-const Stat = chunkify(() => import(/* webpackChunkName: "cms-profile" */ "./components/sections/components/Stat.jsx"));
-const Viz = chunkify(() => import(/* webpackChunkName: "cms-profile" */ "./components/Viz/Viz.jsx"));
-const StatGroup = chunkify(() => import(/* webpackChunkName: "cms-profile" */ "./components/Viz/StatGroup.jsx"));
-export {
-  Profile,
-  Mirror,
-  Section,
-  PDFButton,
-  Selector,
-  Stat,
-  Viz,
-  StatGroup
-};
-
-const Story = chunkify(() => import(/* webpackChunkName: "cms-story" */ "./components/Story.jsx"));
-const StoryLanding = chunkify(() => import(/* webpackChunkName: "cms-story" */ "./components/StoryLanding.jsx"));
-export {Story, StoryLanding};
-
+export {default as Profile} from "./components/Profile.jsx";
+export {default as Mirror} from "./components/Viz/Mirror.jsx";
+export {default as Story} from "./components/Story.jsx";
+export {default as StoryLanding} from "./components/StoryLanding.jsx";
+export {default as Section} from "./components/sections/Section.jsx";
 export {default as Search} from "./components/fields/Search.jsx";
 export {default as ProfileSearch} from "./components/fields/ProfileSearch.jsx";
 export {default as ProfileColumns} from "./components/fields/ProfileColumns.jsx";
 export {default as ProfileTile} from "./components/fields/ProfileTile.jsx";
+
+// Elements to be used by Custom Sections
+export {default as PDFButton} from "./components/sections/components/PDFButton.jsx";
+export {default as Selector} from "./components/sections/components/Selector.jsx";
+export {default as Stat} from "./components/sections/components/Stat.jsx";
+export {default as Viz} from "./components/Viz/Viz.jsx";
+export {default as StatGroup} from "./components/Viz/StatGroup.jsx";
 
 // Redux
 export {default as cmsReducer} from "./reducers/index.js";
