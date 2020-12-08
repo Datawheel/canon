33fb1200af6f35ce6fe244ddebcf2b3973469f32
Dave Landry <landry.dave@gmail.com>
2020-12-08 16:44:18 -0500

    fixes Vizbuilder component styles

--- packages/vizbuilder/src/index.js
+++ packages/vizbuilder/src/index.js
@@ -1,5 +1,3 @@
-import "./styles/index.css";
-
 export {default as Vizbuilder} from "./containers/Vizbuilder";
 export {default as vizbuilderMiddleware} from "./middleware";
 export {vizbuilderReducer} from "./store";
