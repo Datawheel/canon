423880e43d3174d85e9820c6ead2ec5956a7f583
Pablo H. Paladino <paladinop@gmail.com>
2021-05-27 12:26:27 -0300

    send ready state for pm2 proper startup

--- packages/test/src/index.js
+++ packages/test/src/index.js
@@ -3,6 +3,9 @@ const app = require("./app.js");
 
 const port = process.env.CANON_TEST_PORT || 3000;
 
-// Start the server listening for requests
+// Start the server listening for requests & send ready flag
 app.listen(port,
-  () => console.log(`[canon-test] Canon-test server is running in port ${port}`));
+  () => {
+    console.log(`[canon-test] Canon-test server is running in port ${port}`);
+    if (process.send) process.send("ready");
+  });
