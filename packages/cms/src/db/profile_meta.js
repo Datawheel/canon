dcc8ec2eb12b854fd532421c81e5e2b49e1d0e17
jhmullen <jimmy.mullen@gmail.com>
2021-10-12 19:04:33 -0400

    fixes constraint bug with migratino

--- packages/cms/src/db/profile_meta.js
+++ packages/cms/src/db/profile_meta.js
@@ -37,7 +37,7 @@ module.exports = function(sequelize, db) {
   );
 
   p.associate = models => {
-    p.hasMany(models.search, {foreignKey: "cubeName", sourceKey: "cubeName", as: "members"});
+    p.hasMany(models.search, {foreignKey: "cubeName", sourceKey: "cubeName", as: "members", constraints: false});
     p.belongsTo(models.profile, {foreignKey: "id", sourceKey: "profile_id", as: "profile"});
   };
 
