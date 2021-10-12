565a980decd8390c012e8c8b4c26eae691a7a193
jhmullen <jimmy.mullen@gmail.com>
2021-10-12 18:11:39 -0400

    removes erroneous formatter loader

--- packages/cms/scripts/migration/db_0.21/formatter.js
+++ packages/cms/scripts/migration/db_0.21/formatter.js
@@ -31,34 +31,6 @@ module.exports = function(sequelize, db) {
     }
   );
 
-  const folder = path.join(__dirname, "../utils/formatters");
-  f.seed = fs.readdirSync(folder)
-    .filter(file => file && file.indexOf(".") !== 0)
-    .map(file => {
-
-      const fullPath = path.join(folder, file);
-      const content = shell.cat(fullPath).stdout;
-      const name = file.slice(0, -3);
-
-      let description = content.match(/^\/[\s\*\n\r]+([^\*\/]+)/m);
-      if (description) {
-        description = description[1]
-          .replace(/\r/g, " ")
-          .replace(/^[\n\r\s]*/gm, "")
-          .replace(/[\n\r\s]*$/gm, "");
-      }
-      else description = "";
-
-      const logic = content
-        .match(/function[A-z0-9\s\(\)\,]+\{((.|\n|\r)*)\}/m)[1]
-        .replace(/\n\s\s/gm, "\n")
-        .replace(/^[\r|\n]*/gm, "")
-        .replace(/[\n\r\s]*$/gm, "");
-
-      return {name, description, logic};
-
-    });
-
   return f;
 
 };
