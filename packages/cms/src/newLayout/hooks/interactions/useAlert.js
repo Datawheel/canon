d3560055d561f0d54f751eb35bec8fd627a57bf6
jhmullen <jimmy.mullen@gmail.com>
2021-10-29 12:33:43 -0400

    messes with alerts

--- packages/cms/src/newLayout/hooks/interactions/useAlert.js
+++ packages/cms/src/newLayout/hooks/interactions/useAlert.js
@@ -4,26 +4,19 @@
  */
 const {useState, useEffect} = require("react");
 
-/*
-module.exports = alert => {
+module.exports = () => {
   const [showAlert, setShowAlert] = useState(null);
 
-
-  const downHandler = press => {
-    if (press.keyCode === targetKey) setKeyPressed(true);
-  };
-  const upHandler = press => {
-    if (press.keyCode === targetKey) setKeyPressed(false);
-  };
   useEffect(() => {
-    window.addEventListener("keydown", downHandler);
-    window.addEventListener("keyup", upHandler);
-    return () => {
-      window.removeEventListener("keydown", downHandler);
-      window.removeEventListener("keyup", upHandler);
-    };
-  }, []);
-  return keyPressed;
+
+  }, [showAlert]);
+
+  const setAlert = message => {
+    setShowAlert(true);
+    // open alert
+  };
+
+  return [showAlert, setAlert];
 };
-*/
+
 
