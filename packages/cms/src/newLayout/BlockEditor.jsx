9a6d6edd5d0dc1e5d24cc098d618b0b470ef64cb
jhmullen <jimmy.mullen@gmail.com>
2021-11-01 18:06:40 -0400

    adds settings cog to blockeditor

--- packages/cms/src/newLayout/BlockEditor.jsx
+++ packages/cms/src/newLayout/BlockEditor.jsx
@@ -4,12 +4,14 @@ import {Button, Intent} from "@blueprintjs/core";
 import VariableList from "./VariableList";
 import BlockInput from "./blocks/BlockInput";
 import BlockOutput from "./blocks/BlockOutput";
+import BlockSettings from "./blocks/BlockSettings";
 
 import "./BlockEditor.css";
 
 const MODES = {
   INPUT: "input",
-  OUTPUT: "output"
+  OUTPUT: "output",
+  SETTINGS: "settings"
 };
 
 /**
@@ -20,20 +22,24 @@ function BlockEditor({block}) {
   /* state */
   const [mode, setMode] = useState(MODES.INPUT);
 
+  const COMPONENTS = {
+    [MODES.INPUT]: BlockInput,
+    [MODES.OUTPUT]: BlockOutput,
+    [MODES.SETTINGS]: BlockSettings
+  };
+
+  const BlockPanel = COMPONENTS[mode];
+
   return (
     <div className="cms-block-editor">
       <div className="cms-block-editor-toggle">
         <Button onClick={() => setMode(MODES.INPUT)} intent={mode === MODES.INPUT ? Intent.PRIMARY : Intent.NONE} icon="data-lineage">IN</Button>
         <Button onClick={() => setMode(MODES.OUTPUT)}intent={mode === MODES.OUTPUT ? Intent.PRIMARY : Intent.NONE}icon="application">OUT</Button>
+        <Button onClick={() => setMode(MODES.SETTINGS)}intent={mode === MODES.SETTINGS ? Intent.PRIMARY : Intent.NONE}icon="cog"></Button>
       </div>
       <div className="cms-block-editor-content">
         <VariableList />
-        {mode === MODES.INPUT && <React.Fragment>
-          <BlockInput block={block}/>
-        </React.Fragment> }
-        {mode === MODES.OUTPUT && <div className="cms-block-editor-output">
-          <BlockOutput block={block}/>
-        </div>}
+        <BlockPanel block={block} />
       </div>
 
     </div>
---
+++ packages/cms/src/newLayout/blocks/BlockSettings.jsx
@@ -0,0 +1,17 @@
+import React, {useState} from "react";
+import {Button, Intent} from "@blueprintjs/core";
+
+/**
+ *
+ */
+function BlockSettings({block}) {
+
+  return (
+    <div>
+      i'm settings
+    </div>
+  );
+
+}
+
+export default BlockSettings;
