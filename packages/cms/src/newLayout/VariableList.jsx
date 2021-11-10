1f3c029f84b5998ba704de105f1b5b5726123ffd
jhmullen <jimmy.mullen@gmail.com>
2021-11-10 14:34:26 -0500

    allows for sidebar add/remove of inputs

--- packages/cms/src/newLayout/VariableList.jsx
+++ packages/cms/src/newLayout/VariableList.jsx
@@ -2,12 +2,10 @@ import React, {useState} from "react";
 import {Button, Intent} from "@blueprintjs/core";
 import {useDispatch, useSelector} from "react-redux";
 
-import "./VariableList.css";
+import {newEntity, deleteEntity} from "../actions/profiles";
 
-const MODES = {
-  INPUT: "input",
-  OUTPUT: "output"
-};
+import "./VariableList.css";
+import {ENTITY_TYPES} from "../utils/consts/cms";
 
 /**
  *
@@ -17,19 +15,41 @@ function VariableList({id}) {
   const dispatch = useDispatch();
 
   /* redux */
-  const {block, variables} = useSelector(state => {
+  const {block, variables, allVariables, allVariablesSources, allInputs} = useSelector(state => {
     const block = state.cms.profiles.entities.blocks[id];
-    const inputs = Object.values(state.cms.profiles.entities.blocks).filter(d => block.inputs.includes(d.id));
+    const blocks = Object.values(state.cms.profiles.entities.blocks);
+    const allVariables = blocks.reduce((acc, d) => ({...acc, ...d._variables}), {});
+    const allVariablesSources = blocks.reduce((acc, d) => d._variables ? {...acc, ...Object.keys(d._variables).reduce((acc2, d2) => ({...acc2, [d2]: d.id}), {})} : acc, {});
+    const inputs = blocks.filter(d => block.inputs.includes(d.id));
     const variables = inputs.reduce((acc, d) => ({...acc, ...d._variables}), {});
-    return {block, variables};
+    const allInputs = state.cms.profiles.entities.inputs;
+    return {block, variables, allVariables, allVariablesSources, allInputs};
   });
 
   if (!block) return null;
 
+  const onClick = variable => {
+    const id = allVariablesSources[variable];
+    const inUse = variables[variable] !== undefined;
+    if (!inUse) {
+      const payload = {
+        input_id: id,
+        block_id: block.id
+      };
+      dispatch(newEntity(ENTITY_TYPES.BLOCK_INPUT, payload));
+    }
+    else {
+      const payload = {
+        id: Object.values(allInputs).find(d => d.block_input.input_id === id).block_input.id
+      };
+      dispatch(deleteEntity(ENTITY_TYPES.BLOCK_INPUT, payload));
+    }
+  };
+
   return (
     <div className="cms-block-variable-list">
       <ul>
-        {Object.keys(variables).map(d => <li key={d}>{`${d}: ${variables[d]}`}</li>)}
+        {Object.keys(allVariables).map(d => <li key={d} onClick={() => onClick(d)} style={{fontWeight: `${variables[d] ? "bold" : "normal"}`}}>{`${d}: ${allVariables[d]}`}</li>)}
       </ul>
     </div>
   );
