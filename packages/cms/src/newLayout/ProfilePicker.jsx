6c8c845ba80a8a17c0e3d1d8f1781bbdc858ca8a
Dave Landry <landry.dave@gmail.com>
2021-11-16 13:32:44 -0500

    restyles ProfilePicker

--- packages/cms/src/newLayout/ProfilePicker.jsx
+++ packages/cms/src/newLayout/ProfilePicker.jsx
@@ -1,14 +1,12 @@
 import React, {useState, useEffect} from "react";
 import {useDispatch, useSelector} from "react-redux";
-import {ActionIcon, Group} from "@mantine/core";
-import {HiOutlinePlusCircle} from "react-icons/hi";
+import {ActionIcon, Alert, Center, Group, Space} from "@mantine/core";
+import {HiOutlineDocumentReport, HiOutlinePlusCircle} from "react-icons/hi";
 
 import ProfileCard from "./ProfileCard";
 import EntityAddButton from "./components/EntityAddButton";
 
 import {newProfile} from "../actions/profiles";
-
-import "./ProfilePicker.css";
 
 /**
  *
@@ -24,19 +22,29 @@ function ProfilePicker() {
   }));
 
   return (
-    <div className="cms-profile-picker">
-      <h1>Profiles</h1>
-      <Group>
-        {profiles.map(profile =>
-          <ProfileCard key={profile} id={profile} />
-        )}
-        <EntityAddButton
-          label="Profile Name"
-          onSubmit={name => dispatch(newProfile({label: name}))}
-          target={<ActionIcon variant="filled" color="blue" radius="xl" className="cms-profile-new-button"><HiOutlinePlusCircle size={30} /></ActionIcon>}
-        />
+    <Center
+      style={{
+        flexDirection: "column",
+        height: "100vh"
+      }}>
+      <h1>Reports</h1>
+      <Group withGutter position="center" style={{width: "100%"}}>
+        {profiles.length
+          ? profiles.map(profile => <ProfileCard key={profile} id={profile} />)
+          : <Alert
+            icon={<HiOutlineDocumentReport size={22} />}
+            title="No Saved Reports"
+          >
+            Please use the button below to create your first report.
+          </Alert>}
       </Group>
-    </div>
+      <Space size="xl" />
+      <EntityAddButton
+        label="Profile Name"
+        onSubmit={name => dispatch(newProfile({label: name}))}
+        target={<ActionIcon size="xl" radius="xl"><HiOutlinePlusCircle size={30} /></ActionIcon>}
+      />
+    </Center>
   );
 
 }
