2db1081307a34e85145dfd01950477a48fd0bb2f
Dave Landry <landry.dave@gmail.com>
2021-12-07 16:33:03 -0500

    adds Subtitle block renderer

---
+++ packages/reports/src/reports/blocks/types/Subtitle.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import {Text} from "@mantine/core";
+
+import sanitizeBlockContent from "../../../utils/sanitizeBlockContent";
+
+/**
+ * "title" block renderer
+*/
+export default function SubtitlePreview({subtitle}) {
+
+  const subtitleHTML = sanitizeBlockContent(subtitle) ||
+    "<span class='cr-block-placeholder'>Small subtitle.</span>";
+
+  return <Text size="sm" dangerouslySetInnerHTML={{__html: subtitleHTML}} />;
+}
--- packages/reports/src/reports/blocks/types/index.jsx
+++ packages/reports/src/reports/blocks/types/index.jsx
@@ -1,9 +1,11 @@
 import ParagraphPreview from "./Paragraph";
 import StatPreview from "./Stat";
+import SubtitlePreview from "./Subtitle";
 import TitlePreview from "./Title";
 
 export default {
   paragraph: ParagraphPreview,
   stat: StatPreview,
+  subtitle: SubtitlePreview,
   title: TitlePreview
 };
