4300644db2334934268ba23143234952ddfed1b1
Ryan Green <ryan@datawheel.us>
2022-01-11 17:47:02 -0500

    added basic static selector simple UI

--- packages/reports/src/reports/editors/SimpleUI.jsx
+++ packages/reports/src/reports/editors/SimpleUI.jsx
@@ -1,17 +1,34 @@
 /* react */
-import React, {useMemo} from "react";
-
-import {BLOCK_MAP} from "../../utils/consts/cms";
+import React from "react";
+import {BLOCK_TYPES} from "../../utils/consts/cms";
+import SelectorUI from "./simple/SelectorUI";
 
 /**
- *
+ * Props expected by all SimpleUI-type components.
+ * @typedef {Object} SimpleUIProps
+ * @property {(logic: string, simpleState: string, locale: string) => void} onChange - callback for handling
+ * @property {string} type - type of block being edited (BLOCK_TYPES)
+ * @property {Object} simpleState - saved state of simple UI form
  */
-function SimpleUI({type, onChange, locale}) {
 
-  return (
-    <div>SIMPLEUI COMING SOON</div>
-  );
+const SIMPLE_UI_COMPONENT_TYPES = {
+  [BLOCK_TYPES.SELECTOR]: SelectorUI
+};
 
-}
+const getSimpleUIComponent = props => {
+  const Comp = SIMPLE_UI_COMPONENT_TYPES[props.type];
+  if (!Comp) console.error(`No SimpleUI component for block type ${props.type}`);
+  return Comp && <Comp {...props}/>;
+};
+
+/**
+ * Generalized wrapper component that renders a simple GUI editor
+ * for the appropriate block type
+ * @param {SimpleUIProps} props
+ */
+const SimpleUI = props =>
+  <>
+    {getSimpleUIComponent(props)}
+  </>;
 
 export default SimpleUI;
