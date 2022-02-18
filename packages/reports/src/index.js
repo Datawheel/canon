10777538224ba1be2f191f78271e4095ccb3738b
jhmullen <jimmy.mullen@gmail.com>
2022-02-18 16:32:53 -0500

    adds dead simple front-end

--- packages/reports/src/index.js
+++ packages/reports/src/index.js
@@ -2,10 +2,12 @@ import {chunkify} from "@datawheel/canon-core";
 
 const ReportBuilder = chunkify(/* #__LOADABLE__ */ () => import(/* webpackChunkName: "report-builder" */ "./ReportBuilder.jsx"));
 const MemberEditor = chunkify(/* #__LOADABLE__ */ () => import(/* webpackChunkName: "member-editor" */ "./reports/members/MemberEditor"));
+const Report = chunkify(/* #__LOADABLE__ */ () => import(/* webpackChunkName: "member-editor" */ "./frontend/Report"));
 
 export {
   MemberEditor,
-  ReportBuilder
+  ReportBuilder,
+  Report
 };
 
 // Redux
