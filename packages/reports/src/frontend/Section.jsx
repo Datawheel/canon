596a8b2bf35c6a84abd92d65214f34569bdb93b5
jhmullen <jimmy.mullen@gmail.com>
2022-02-23 13:46:55 -0500

    fixes selectors on front-end

--- packages/reports/src/frontend/Section.jsx
+++ packages/reports/src/frontend/Section.jsx
@@ -4,18 +4,22 @@ import React from "react";
 /** type-specific render components */
 import TypeRenderers from "../reports/blocks/types/renderers";
 
+import {BLOCK_TYPES} from "../utils/consts/cms";
+
 /**
  *
  */
 function Section({content}) {
 
-  const {blocks} = content;
+  const blocks = content.blocks.filter(d => d.type !== BLOCK_TYPES.GENERATOR);
 
   return (
     <div className="cms-section-content">
       {blocks.map(block => {
         const Renderer = TypeRenderers[block.type];
-        return <Renderer key={block.id} {...block.renderContent} />;
+        // todo1.0 fix this as part of ryan's rewrite
+        const payload = block.type === BLOCK_TYPES.SELECTOR ? {config: block.renderContent} : block.renderContent;
+        return <Renderer key={block.id} {...payload} />;
       })}
     </div>
   );
