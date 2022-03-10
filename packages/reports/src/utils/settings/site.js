34bc9ae3e96e43128868483423dd77da609bd0a7
Dave Landry <landry.dave@gmail.com>
2022-03-10 17:26:21 -0500

    creates global site styles

---
+++ packages/reports/src/utils/settings/site.js
@@ -0,0 +1,16 @@
+export default {
+  backgroundColor: "#fafafa",
+  block: {
+    padding: 10
+  },
+  column: {
+    padding: 12
+  },
+  section: {
+    backgroundColor: "white",
+    borderRadius: 10,
+    boxShadow: "0px 6px 10px rgba(12, 32, 50, 0.14), 0px 1px 15px rgba(12, 32, 50, 0.12), 0px 3px 5px rgba(12, 32, 50, 0.15)",
+    margin: 16,
+    padding: 32
+  }
+};
