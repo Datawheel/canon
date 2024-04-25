425dbb5c151e26b1974a0568f2ccabbfa76cfe5d
Walter Buczacka <walterbuczacka@Walters-MacBook-Pro.local>
2024-04-25 11:04:11 -0300

    add usecallback hook to not createa new function on each render

--- packages/next/cms/hooks/useFocus.js
+++ packages/next/cms/hooks/useFocus.js
@@ -1,10 +1,10 @@
-import {useRef} from "react";
+import {useCallback, useRef} from "react";
 
 const useFocus = () => {
   const htmlElRef = useRef(null);
-  const setFocus = () => {
+  const setFocus = useCallback(() => {
     if (htmlElRef.current) htmlElRef.current.focus();
-  };
+  }, []);
 
   return [htmlElRef, setFocus];
 };
